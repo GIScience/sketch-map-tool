c9e3666cb0e76444a5f956308037eed12cb1d1d9
Matthias Schaub <matthias.schaub@heigit.org>
2022-09-19 16:16:23 +0200

    fix: update smt version to 0.9.0 in pyproject.toml

---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+import os
+
+import toml
+from sketch_map_tool import __version__ as version
+
+
+def test_version():
+    infile = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..", "pyproject.toml"
+    )
+    with open(infile, "r") as fo:
+        project_file = toml.load(fo)
+        pyproject_version = project_file["tool"]["poetry"]["version"]
+    assert pyproject_version == version
