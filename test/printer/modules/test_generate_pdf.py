e38a0a0740c2b6a8753c0adce6626f18dfa3a48f
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2022-10-07 13:20:31 +0200

    Complete test_generate_pdf and add test data

--- test/printer/modules/test_generate_pdf.py
+++ test/printer/modules/test_generate_pdf.py
@@ -2,39 +2,60 @@
 Tests for the module printer/modules/generate_pdf.py
 """
 import os
+import filecmp
+
 import pytest
+import fitz
 from PIL import Image
+
 from helper_modules.bbox_utils import Bbox
 from printer.modules import generate_pdf
-from printer.modules.paper_formats.paper_formats import (A0, A1, A2, A3, A4, A5, LEGAL, LETTER,
-                                                         LEDGER, TABLOID, PaperFormat)
+from printer.modules.paper_formats.paper_formats import (A0,
+                                                         A1,
+                                                         A2,
+                                                         A3,
+                                                         A4,
+                                                         A5,
+                                                         LEGAL,
+                                                         LETTER,
+                                                         LEDGER,
+                                                         TABLOID,
+                                                         PaperFormat)
 
-OUTPUT_DIR = "./test_output/"  # Will be temporarily created and removed after the tests
+OUTPUT_DIR = "./test_output/"
 DUMMY_BBOX = Bbox.bbox_from_str("8.66100311,49.3957813,8.71662140,49.4265373")
 
 generate_pdf.RESOURCE_PATH = "../../../printer/modules/resources/"
 
 
-@pytest.mark.parametrize("paper_format", [A0, A1, A2, A3, A4, A5, LEGAL, LETTER, LEDGER, TABLOID])
-def test_generate_pdf_landscape(paper_format: PaperFormat) -> None:
+@pytest.mark.parametrize("paper_format",
+                         [A0, A1, A2, A3, A4, A5, LEGAL, LETTER, LEDGER,
+                          TABLOID])
+@pytest.mark.parametrize("orientation", ["landscape", "portrait"])
+def test_generate_pdf(paper_format: PaperFormat, orientation: str) -> None:
     """
-    Test for the function generate_pdf with a map image causing the sketch map to be in landscape
-    orientation.
+    Test for the function generate_pdf with a map image causing the sketch map
+    to be in landscape or portrait orientation.
     """
-    os.mkdir(OUTPUT_DIR)
-    map_image = Image.open("../test_data/dummy_map_img_landscape.jpg")
+    map_image = Image.open(f"../test_data/dummy_map_img_{orientation}.jpg")
     result_path = generate_pdf.generate_pdf(OUTPUT_DIR,
                                             map_image,
                                             DUMMY_BBOX,
                                             "2021-12-24",
                                             paper_format)
-    os.remove(OUTPUT_DIR)
-    # TODO: Compare resulting PDF with expected one under test_data/expected/generate_pdf/
-
-
-def test_generate_pdf_portrait() -> None:
-    """
-    Test for the function generate_pdf with a map image causing the sketch map to be in portrait
-    orientation.
-    """
-    pass  # TODO
+    result_template_path = result_path.replace(".pdf", "_template.jpg")
+    fitz_pdf = fitz.open(result_path)
+    pdf_img_path = result_path.replace(".pdf", ".jpg")
+    fitz_pdf[0].get_pixmap().save(pdf_img_path)
+    fitz_pdf.close()
+    assert filecmp.cmp(
+        pdf_img_path,
+        f"../test_data/expected/{orientation}/{paper_format}.jpg",
+        shallow=False)
+    assert filecmp.cmp(
+        result_template_path,
+        f"../test_data/expected/{orientation}/{paper_format}_template.jpg",
+        shallow=False)
+    os.remove(result_path)
+    os.remove(pdf_img_path)
+    os.remove(result_template_path)
