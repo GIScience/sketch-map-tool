1a29cc5f16a2f4d7f6c00dd53533f03de3195d04
Michael Auer <michael.auer@heigit.org>
2022-10-12 17:17:24 +0200

    feat(flask): parse html form as json
    style: run isort and black formatters

--- sketch_map_tool/app2.py
+++ sketch_map_tool/app2.py
@@ -1,37 +1,40 @@
-from flask import Flask, request, redirect, url_for, render_template
-from uuid import uuid4, UUID
+import json
+from uuid import UUID, uuid4
 
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 
-@app.get('/')
+@app.get("/")
 def index():
     return render_template("index2.html")
 
 
-@app.get('/create')
+@app.get("/create")
 def create():
     """Serve forms for creating a sketch map"""
-    return render_template('create.html')
+    return render_template("create.html")
 
 
-@app.post('/create/results')
+@app.post("/create/results")
 def create_results_post():
     """Create the sketch map"""
-    request.form['bbox']
-    request.form['format']
-    request.form['orientation']
-    request.form['size']
+    request.form["bbox"]
+    request.form["format"]
+    request.form["orientation"]
+    size = json.loads(request.form["size"])
+    print(request.form)
+    print(size)
     uuid = uuid4()
-    return render_template("create-results.html")
+    return render_template("create-results.html", uuid=uuid)
 
 
 @app.get('/create/results')
 @app.get('/create/results/<uuid>')
 def create_results_get(uuid=None):
     if uuid is None:
-        return redirect(url_for('create'))
+        return redirect(url_for("create"))
 
     try:
         uuid = UUID(uuid, version=4)
@@ -40,10 +43,6 @@ def create_results_get(uuid=None):
     return render_template("create-results.html")
 
 
-@app.get('/create/results/status/<uuid>')
+@app.get("/create/results/status/<uuid>")
 def create_results_status(uuid):
-    return {
-        "id": uuid,
-        "status": "computing",
-        "progress": 0.5
-    }
+    return {"id": uuid, "status": "computing", "progress": 0.5}
--- tests/test_app_create.py
+++ tests/test_app_create.py
@@ -1,5 +1,6 @@
-import pytest
 from uuid import UUID
+
+import pytest
 
 from sketch_map_tool.app2 import app
 
