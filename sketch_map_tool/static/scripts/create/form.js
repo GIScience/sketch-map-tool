b85821e4b886c15d7e9820ebfc8340b880558561
Matthias Schaub <matthias.schaub@heigit.org>
2022-10-19 12:50:04 +0200

    style: run `eslint --fix`

--- sketch_map_tool/static/scripts/create/form.js
+++ sketch_map_tool/static/scripts/create/form.js
@@ -1,35 +1,34 @@
 function fillSelectOptions(selectElementId, optionsMap) {
     const selectElement = document.getElementById(selectElementId);
     for (const paperformatKey in optionsMap) {
-        const option = document.createElement('option');
+        const option = document.createElement("option");
         option.text = optionsMap[paperformatKey];
         selectElement.appendChild(option);
     }
 }
 
 // property: format
-fillSelectOptions('format', PAPER_FORMAT);
+fillSelectOptions("format", PAPER_FORMAT);
 
 // set initial form value from ol-control
-document.getElementById('format').value = printLayoutControl.getFormat();
+document.getElementById("format").value = printLayoutControl.getFormat();
 
 // bind listeners to update ol-control from form
-document.getElementById('format').addEventListener('change', (event) => {
+document.getElementById("format").addEventListener("change", (event) => {
     const format = event.target.value;
     const orientation = printLayoutControl.getOrientation();
     printLayoutControl.setFormat(format);
     printLayoutControl.setMargin(new ol.control.PrintLayout.Margin(SKETCH_MAP_MARGINS[format][orientation]));
 });
 
-
 // property: orientation
-fillSelectOptions('orientation', ORIENTATION);
+fillSelectOptions("orientation", ORIENTATION);
 
 // set initial form value from ol-control
-document.getElementById('orientation').value = printLayoutControl.getOrientation();
+document.getElementById("orientation").value = printLayoutControl.getOrientation();
 
 // bind listeners to update ol-control from form
-document.getElementById('orientation').addEventListener('change', (event) => {
+document.getElementById("orientation").addEventListener("change", (event) => {
     const orientation = event.target.value;
     const format = printLayoutControl.getFormat();
     printLayoutControl.setOrientation(orientation);
@@ -37,15 +36,15 @@ document.getElementById('orientation').addEventListener('change', (event) => {
 });
 
 // property: bbox (in webmercator)
-document.getElementById('bbox').value = JSON.stringify(printLayoutControl.getBbox());
-printLayoutControl.on('change:bbox', (event)=>{
+document.getElementById("bbox").value = JSON.stringify(printLayoutControl.getBbox());
+printLayoutControl.on("change:bbox", (event) => {
     const newBbox = event.target.getBbox();
-    document.getElementById('bbox').value = JSON.stringify(newBbox);
+    document.getElementById("bbox").value = JSON.stringify(newBbox);
 });
 
 // property: size (WMS request - width and height params)
-document.getElementById('size').value = JSON.stringify(printLayoutControl.getPrintBoxSizeInDots(192));
-printLayoutControl.on('change', (event)=>{
+document.getElementById("size").value = JSON.stringify(printLayoutControl.getPrintBoxSizeInDots(192));
+printLayoutControl.on("change", (event) => {
     const newSize = event.target.getPrintBoxSizeInDots(192);
-    document.getElementById('size').value = JSON.stringify(newSize);
+    document.getElementById("size").value = JSON.stringify(newSize);
 });
