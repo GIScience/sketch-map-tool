b371f97acaad41be84d2f728688c1ec416aa7225
Matthias Schaub <matthias.schaub@heigit.org>
2022-10-19 12:50:04 +0200

    style: run `eslint --fix`

--- sketch_map_tool/static/scripts/create/map.js
+++ sketch_map_tool/static/scripts/create/map.js
@@ -1,19 +1,19 @@
 const map = new ol.Map({
-    target: 'map',
+    target: "map",
     view: new ol.View({
         center: ol.proj.fromLonLat([8.68, 49.41]),
-        zoom: 15
+        zoom: 15,
     }),
     layers: [
         new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ]
+            source: new ol.source.OSM(),
+        }),
+    ],
 });
 
 const printLayoutControl = new ol.control.PrintLayout({
     format: PAPER_FORMAT.A4,
     orientation: ORIENTATION.LANDSCAPE,
-    margin: SKETCH_MAP_MARGINS.A4.landscape
+    margin: SKETCH_MAP_MARGINS.A4.landscape,
 });
 map.addControl(printLayoutControl);
