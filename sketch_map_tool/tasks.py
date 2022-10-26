05bf820da9a4c29029721d34c0dcb0e6021cfa4f
Matthias Schaub <matthias.schaub@heigit.org>
2022-10-26 15:22:04 +0200

    feat(celery): simulate long running tasks
    for sketch map and quality report generation

--- sketch_map_tool/tasks.py
+++ sketch_map_tool/tasks.py
@@ -1,7 +1,15 @@
+from time import sleep
+
 from sketch_map_tool import celery_app as celery
 
 
 @celery.task()
-def generate_sketchmap(bbox: list, format_: str, orientation: str, size: dict) -> str:
-    print("celery task")
-    return "Done"
+def generate_sketch_map(bbox: list, format_: str, orientation: str, size: dict) -> str:
+    sleep(10)  # simulate long running task (10s)
+    return "Sketch Map"
+
+
+@celery.task()
+def generate_quality_report(bbox: list) -> str:
+    sleep(10)  # simulate long running task (10s)
+    return "Quality Report"
