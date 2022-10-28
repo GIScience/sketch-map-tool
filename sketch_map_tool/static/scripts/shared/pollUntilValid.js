9ea076c03adf532aa04719a8e3c848b684e8e8db
Michael Auer <michael.auer@heigit.org>
2022-10-28 11:17:20 +0200

    change validation logic and signature of poll function and add documentation

--- sketch_map_tool/static/scripts/shared/pollUntilValid.js
+++ sketch_map_tool/static/scripts/shared/pollUntilValid.js
@@ -1,21 +1,30 @@
 class PollUntilValid {
     /**
+     * The poll method will repeatedly fetch data from the given URL in the specified amount of
+     * interval time.
+     * It will do so as long as the validate function does not return "true" or an http code
+     * indicates problems.
+     * To catch Network errors you should wrap the function call in a try-catch statement.
      *
-     * @param url
-     * @param fnCondition
-     * @param onProgress callback if
-     * @param onValid callback
-     * @param onError callback
-     * @param intervalInMilliseconds
-     * @returns {Promise<Response>}
+     * @param {string} url - The URL to poll from
+     * @param {function(Response)} validateFn - Validation function. Gets the response as param.
+     *     Should return true when the response meets the requirements, so it will stop the polling.
+     * @param {int} [intervalInMilliseconds=1000] - The amount of time waiting until
+     *     the next request is sent
+     * @param {function(Response)} [onValid] - opt. callback. Do something with the valid response.
+     * @param {function(Response)} [onProgress] - opt. callback to act on "invalid"
+     *     e.g. progess responses
+     * @param {function(Response)} [onError] - opt. callback to react on errors
+     *     (http status codes other than 200-299)
+     * @returns {Promise<Response>} - The last "valid" response. Use this or the onValid callback.
      */
     static async poll(
         url,
-        fnCondition,
-        onProgress,
-        onValid,
-        onError,
+        validateFn = () => true,
         intervalInMilliseconds = 1000,
+        onValid = () => {},
+        onProgress = () => {},
+        onError = () => {},
     ) {
         let response = await fetch(url);
 
@@ -28,7 +37,7 @@ class PollUntilValid {
             throw new Error(`The server responded with an Error: ${response.status} ${response.statusText}`);
         }
 
-        while (fnCondition(response)) {
+        while (!validateFn(response)) {
             /* eslint-disable no-await-in-loop */
             await onProgress(response);
             await PollUntilValid.wait(intervalInMilliseconds);
