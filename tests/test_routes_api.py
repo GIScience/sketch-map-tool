e443f1c53a09351428edfc4c785fe04980556470
Matthias Schaub <matthias.schaub@heigit.org>
2022-10-31 13:43:44 +0100

    test: add tests for api endpoint /status

--- tests/test_routes_api.py
+++ tests/test_routes_api.py
@@ -1,6 +1,16 @@
-from uuid import UUID
+import json
 
 import pytest
+from celery.states import (
+    FAILURE,
+    PENDING,
+    RECEIVED,
+    REJECTED,
+    RETRY,
+    REVOKED,
+    STARTED,
+    SUCCESS,
+)
 
 from sketch_map_tool.routes import app
 
@@ -10,21 +20,86 @@ def client():
     return app.test_client()
 
 
-def test_create_results_status(client):
-    uuid = "16fd2706-8baf-433b-82eb-8c7fada847da"
-    resp = client.get("/api/status/{0}".format(uuid))
+@pytest.fixture()
+def uuid():
+    return "16fd2706-8baf-433b-82eb-8c7fada847da"
+
+
+@pytest.fixture()
+def mock_request_task_mapping(request, monkeypatch):
+    """Mock request id to task id mapping."""
+    type_ = request.getfixturevalue("type_")
+    request_task = json.dumps({type_: "16fd2706-8baf-433b-82eb-8c7fada847da"})
+    monkeypatch.setattr("sketch_map_tool.routes.ds_client.get", lambda x: request_task)
+
+
+@pytest.fixture()
+def mock_async_results(request, monkeypatch):
+    """Mock celery tasks results."""
+
+    class MockTask:
+        def __init__(self, status):
+            self.status = status
+
+    status = request.getfixturevalue("status")
+    mock_task = MockTask(status)
+    monkeypatch.setattr(
+        "sketch_map_tool.routes.tasks.generate_quality_report.AsyncResult",
+        lambda x: mock_task,
+    )
+    monkeypatch.setattr(
+        "sketch_map_tool.routes.tasks.generate_sketch_map.AsyncResult",
+        lambda x: mock_task,
+    )
+
+
+@pytest.mark.parametrize("status", (SUCCESS,))
+@pytest.mark.parametrize("type_", ("sketch-map", "quality-report"))
+def test_status_success(
+    client,
+    uuid,
+    type_,
+    status,
+    mock_request_task_mapping,
+    mock_async_results,
+):
+    resp = client.get("/api/status/{0}/{1}".format(uuid, type_))
     assert resp.status_code == 200
-
-    try:
-        UUID(resp.json["id"])
-    except ValueError:
-        assert "invalid uuid"
-
-    assert isinstance(resp.json["status"], str)
+    assert resp.mimetype == "application/json"
+    assert resp.json["id"] == uuid
+    assert resp.json["status"] == "SUCCESS"
+    assert resp.json["href"] == "/api/download/{0}/{1}".format(uuid, type_)
 
 
-def test_download(client):
-    resp = client.get("/api/download")
-    assert resp.status_code == 200
-    assert resp.mimetype == "application/pdf"
-    assert resp.data == b"Mock PDF\n"
+@pytest.mark.parametrize("status", (PENDING, RETRY, RECEIVED, STARTED))
+@pytest.mark.parametrize("type_", ("sketch-map", "quality-report"))
+def test_status_processing(
+    client,
+    uuid,
+    type_,
+    status,
+    mock_request_task_mapping,
+    mock_async_results,
+):
+    resp = client.get("/api/status/{0}/{1}".format(uuid, type_))
+    assert resp.status_code == 202
+    assert resp.json["id"] == uuid
+    assert resp.json["status"] == str(status)
+    assert "href" not in resp.json.keys()
+
+
+@pytest.mark.parametrize("status", (REJECTED, REVOKED, FAILURE))
+@pytest.mark.parametrize("type_", ("sketch-map", "quality-report"))
+def test_status_failure(
+    client,
+    uuid,
+    type_,
+    status,
+    mock_request_task_mapping,
+    mock_async_results,
+):
+    resp = client.get("/api/status/{0}/{1}".format(uuid, type_))
+    assert resp.status_code == 500
+    assert resp.json["id"] == uuid
+    assert resp.json["status"] == str(status)
+    assert "href" not in resp.json.keys()
