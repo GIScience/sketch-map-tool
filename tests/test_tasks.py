a66d357ff1ac3e4f058c2fb47f60044698d78a86
Matthias Schaub <matthias.schaub@heigit.org>
2022-10-31 10:49:19 +0100

    test: add tests for tasks module

---
+++ tests/test_tasks.py
@@ -0,0 +1,17 @@
+"""Test tasks without using the tasks queue Celery."""
+
+from io import BytesIO
+
+from sketch_map_tool import tasks
+
+
+def test_generate_sketch_map(monkeypatch):
+    monkeypatch.setattr("sketch_map_tool.tasks.sleep", lambda x: None)
+    result = tasks.generate_sketch_map([], "", "", {})
+    assert isinstance(result, BytesIO)
+
+
+def test_generate_quality_report(monkeypatch):
+    monkeypatch.setattr("sketch_map_tool.tasks.sleep", lambda x: None)
+    result = tasks.generate_quality_report([])
+    assert isinstance(result, BytesIO)
