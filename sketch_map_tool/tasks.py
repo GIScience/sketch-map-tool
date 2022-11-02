11a2851c360090577a5801961450d05105920967
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-02 13:00:24 +0100

    feat(celery): bind task to get access to task id

--- sketch_map_tool/tasks.py
+++ sketch_map_tool/tasks.py
@@ -9,14 +9,16 @@ from reportlab.pdfgen import canvas
 from sketch_map_tool import celery_app as celery
 
 
-@celery.task()
+@celery.task(bind=True)
 def generate_sketch_map(
+    self,
     bbox: List[float],
     format_: str,
     orientation: str,
     size: Dict[str, float],
 ) -> Union[BytesIO, AsyncResult]:
     """Generate a sketch map as PDF."""
+    print(self.request.id)
     sleep(5)  # simulate long running task (5s)
     buffer = BytesIO()
     canv = canvas.Canvas(buffer, pagesize=A4)
@@ -26,12 +28,16 @@ def generate_sketch_map(
     return buffer
 
 
-@celery.task()
-def generate_quality_report(bbox: List[float]) -> Union[str, AsyncResult]:
+@celery.task(bind=True)
+def generate_quality_report(
+    self,
+    bbox: List[float],
+) -> Union[BytesIO, AsyncResult]:
     """Generate a quality report as PDF.
 
     Fetch quality indicators from the OQT API
     """
+    print(self.request.id)
     sleep(10)  # simulate long running task (10s)
     buffer = BytesIO()
     canv = canvas.Canvas(buffer, pagesize=A4)
