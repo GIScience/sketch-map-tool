5f617dc3b1d157423b37ed2752e7bbd3410d934e
Michael Auer <michael.auer@heigit.org>
2022-11-02 10:12:55 +0100

    [WIP] add filebokz

---
+++ client-src/digitize/index.js
@@ -0,0 +1,5 @@
+import "filebokz/dist/filebokz.css";
+import "filebokz/dist/filebokz-theme.css";
+import filebokz from "filebokz";
+
+filebokz();
--- esbuild.js
+++ esbuild.js
@@ -5,6 +5,7 @@ require("esbuild")
             "./client-src/index/index.css",
             "./client-src/create/index.js",
             "./client-src/create-results/index.js",
+            "./client-src/digitize/index.js",
         ],
         entryNames: "[dir]", // will name the result files by their folder names
         outbase: "./client-src",
