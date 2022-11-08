412a456e58d57fbb5893f94e65d82bc650697a4c
Michael Auer <michael.auer@heigit.org>
2022-11-08 15:29:24 +0100

    include uploaded image in celery task result pdf mock

--- sketch_map_tool/tasks.py
+++ sketch_map_tool/tasks.py
@@ -4,8 +4,8 @@ from typing import Dict, List, Union
 
 from celery.result import AsyncResult
 from reportlab.lib.pagesizes import A4
+from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen import canvas
-from werkzeug.datastructures import FileStorage
 
 from sketch_map_tool import celery_app as celery
 from sketch_map_tool.wms import client as wms_client
@@ -50,15 +50,15 @@ def generate_quality_report(
 
 
 @celery.task(bind=True)
-def generate_digitized_results(
-    self, files: List[FileStorage]
-) -> Union[BytesIO, AsyncResult]:
+def generate_digitized_results(self, files) -> Union[BytesIO, AsyncResult]:
     """Generate first raster data, then vector data and finally a QGIS project"""
     print(self.request.id)
+    print(files[0]["filename"])
     sleep(3)  # simulate long running task (3s)
     buffer = BytesIO()
     canv = canvas.Canvas(buffer, pagesize=A4)
     canv.drawString(100, 100, "Digitized Results")
+    canv.drawImage(ImageReader(files[0]["bytes"]), 100, 120, 400, None, None, True)
     canv.save()
     buffer.seek(0)
     return buffer
