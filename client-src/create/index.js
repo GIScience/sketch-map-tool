b86948e5305875647ddb786fd7ab3573af1f2afe
Michael Auer <michael.auer@heigit.org>
2022-11-08 22:03:38 +0100

    add geocoder to ol-map at /create
    closes #48

--- client-src/create/index.js
+++ client-src/create/index.js
@@ -1,10 +1,13 @@
 import "ol/ol.css";
 import "@giscience/ol-print-layout-control/dist/ol-print-layout-control.css";
+import "@kirtandesai/ol-geocoder/dist/ol-geocoder.css";
+import "./geocoder.css";
 import "./create.css";
 
-import { createMap, addPrintLayoutControl } from "./map.js";
+import { createMap, addPrintLayoutControl, addGeocoderControl } from "./map.js";
 import { bindFormToPrintLayoutControl } from "./form.js";
 
 const map = createMap();
 const printLayoutControl = addPrintLayoutControl(map);
 bindFormToPrintLayoutControl(printLayoutControl);
+addGeocoderControl(map);
