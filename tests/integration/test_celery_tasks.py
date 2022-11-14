f29ef48bd41d1727e57f0dc6c8d9b50a537001f7
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-14 15:51:32 +0100

    tests: fix integration tests

--- tests/integration/test_celery_tasks.py
+++ tests/integration/test_celery_tasks.py
@@ -3,8 +3,10 @@ from io import BytesIO
 from sketch_map_tool import tasks
 
 
-def test_generate_sketch_map(celery_worker, bbox, format_, size):
-    task = tasks.generate_sketch_map.apply_async(args=(bbox, format_, "landscape", size, 0.0))
+def test_generate_sketch_map(celery_worker, bbox, format_, size, scale):
+    task = tasks.generate_sketch_map.apply_async(
+        args=(bbox, format_, "landscape", size, scale)
+    )
     result = task.wait()
     assert isinstance(result, BytesIO)
 
