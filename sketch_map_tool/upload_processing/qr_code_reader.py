b4038bc2308040e7741e62cc7ad2cffacc212210
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-16 12:45:51 +0100

    feat(qr-code): decode data to Python objects
    and fix tests

--- sketch_map_tool/upload_processing/qr_code_reader.py
+++ sketch_map_tool/upload_processing/qr_code_reader.py
@@ -2,16 +2,21 @@
 Read QR codes from photos / scans
 """
 
+import json
+from json import JSONDecodeError
+from types import MappingProxyType
 from typing import Any
 
 import cv2
 import numpy as np
 from pyzbar import pyzbar
 
+from sketch_map_tool import definitions
 from sketch_map_tool.exceptions import QRCodeError
+from sketch_map_tool.models import Bbox, Size
 
 
-def read(img: "np.ndarray[Any, np.dtype[np.int64]]", depth=0) -> str:
+def read(img: "np.ndarray[Any, np.dtype[np.int64]]", depth=0) -> MappingProxyType:
     """Detect and decode QR-Code.
 
     If QR-Code is falsely detected but no data exists recursively down scale QR-Code
@@ -19,9 +24,10 @@ def read(img: "np.ndarray[Any, np.dtype[np.int64]]", depth=0) -> str:
 
     :param image: Image containing one QR code.
     :param depth: Maximal recursion depth
-    :return: Contents of the QR code.
-    :raises QRCodeError: If no QR-code could be detected or if multiple QR-codes have
-        been detected.
+    :return: Contents of the QR-Code.
+    :raises QRCodeError: If no QR-Code could be detected
+        or if multiple QR-Codes have been detected
+        or if QR-Code does not have expected content
     """
     decoded_objects: list = pyzbar.decode(img)
     match len(decoded_objects):
@@ -32,9 +38,29 @@ def read(img: "np.ndarray[Any, np.dtype[np.int64]]", depth=0) -> str:
             else:
                 raise QRCodeError("QR-Code could not be detected.")
         case 1:
-            return decoded_objects[0].data.decode()
+            data = decoded_objects[0].data.decode()
+            return _decode_data(data)
         case _:
             raise QRCodeError("Multiple QR-Codes detected.")
+
+
+def _decode_data(data) -> MappingProxyType:
+    try:
+        d = json.loads(data)
+    except JSONDecodeError as error:
+        raise QRCodeError("QR-Code does not have expected content.") from error
+    try:
+        return MappingProxyType(
+            {
+                "uuid": d["id"],
+                "bbox": Bbox(**d["bbox"]),
+                "format_": getattr(definitions, d["format"].upper()),
+                "orientation": d["orientation"],
+                "size": Size(**d["size"]),
+            }
+        )
+    except KeyError as error:
+        raise QRCodeError("QR-Code does not have expected content.") from error
 
 
 def _resize(img, scale: float = 0.75):
--- tests/unit/test_qr_code_reader.py
+++ tests/unit/test_qr_code_reader.py
@@ -1,3 +1,5 @@
+from types import MappingProxyType
+
 import cv2
 import pytest
 
@@ -13,9 +15,10 @@ def qr_code_img():
     return cv2.imread(str(QR_CODES_FIXTURE_DIR / "qr-code.png"))
 
 
-@pytest.fixture
-def qr_code_img_big():
-    return cv2.imread(str(QR_CODES_FIXTURE_DIR / "qr-code-big.png"))
+# TODO: Find fixture which need to be downscaled
+# @pytest.fixture
+# def qr_code_img_big():
+#     return cv2.imread(str(QR_CODES_FIXTURE_DIR / "qr-code-sketch-map-big.png"))
 
 
 @pytest.fixture
@@ -33,28 +36,38 @@ def qr_code_sketch_map():
     return cv2.imread(str(QR_CODES_FIXTURE_DIR / "qr-code-sketch-map.png"))
 
 
-def test_read_qr_code(qr_code_img):
+@pytest.fixture
+def decoded_content(format_, bbox, size):
+    return MappingProxyType(
+        {
+            "uuid": "5f3d1d79-833a-44a6-9ab0-840081e40d43",
+            "bbox": bbox,
+            "format_": format_,
+            "orientation": "landscape",
+            "size": size,
+        }
+    )
+
+
+def test_read_qr_code(qr_code_img, decoded_content):
     # Too manually check the image uncomment following code.
     # cv2.imshow('image', qr_code_img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    assert qr_code_reader.read(qr_code_img) == "Sketch Map Tool"
+    assert qr_code_reader.read(qr_code_img) == decoded_content
 
 
-def test_read_qr_code_sketch_map(qr_code_sketch_map):
-    assert (
-        qr_code_reader.read(qr_code_sketch_map)
-        == '90b3345b-36cf-42ef-8bcc-c1b4cf0f8473{"lat_min": 965015.9352927138, "lon_min": 6344342.122209794, "lat_max": 967072.4894825463, "lon_max": 6346090.619231817}a4landscape{"width": 1867, "height": 1587}0.9.02022-11-14T15:54:36.109466+00:00'
-    )
+def test_read_qr_code_sketch_map(qr_code_sketch_map, decoded_content):
+    assert qr_code_reader.read(qr_code_sketch_map) == decoded_content
 
 
-# TODO: Fixture does not need to be down scaled.
-def test_read_qr_code_big(qr_code_img_big):
-    """Test reading a QR-Code image which size is too big and need to be down-scaled ...
+# TODO: Find fixture which need to be downscaled
+# def test_read_qr_code_big(qr_code_img_big):
+#     """Test reading a QR-Code image which size is too big and need to be down-scaled ...
 
-    ... before content can be detected.
-    """
-    assert qr_code_reader.read(qr_code_img_big) == "Sketch Map Tool"
+#     ... before content can be detected.
+#     """
+#     result = qr_code_reader.read(qr_code_img_big)
 
 
 def test_read_qr_code_multiple(qr_code_img_mutliple):
