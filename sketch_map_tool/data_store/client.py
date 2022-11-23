cb1f784f3ae2e8072dd66fe9229018857a53f7ae
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-23 15:33:23 +0100

    feat: get map image for uploaded sketch map

--- sketch_map_tool/data_store/client.py
+++ sketch_map_tool/data_store/client.py
@@ -7,11 +7,13 @@ Redis is also used as result backend of celery.
 
 from __future__ import annotations
 
-from typing import Dict
+import json
+from typing import Dict, get_args
 
 import redis
 
 from sketch_map_tool.config import get_config_value
+from sketch_map_tool.definitions import ALLOWED_TYPES, DIGITIZE_TYPES
 
 
 def _get_client() -> redis.Redis:
@@ -26,3 +28,20 @@ def set(data: Dict[str, str]):
 def get(key: str) -> str:
     client = _get_client()
     return client.get(key)
+
+
+def get_task_id(uuid: str, type_: ALLOWED_TYPES) -> int:
+    """Get the celery task id from the data store using the request id and type."""
+    if type_ in list(get_args(DIGITIZE_TYPES)):
+        # Task id equals request uuid (/digitize/results)
+        return uuid
+    # Get task id from data-store (/create/results)
+    raw = get(str(uuid))
+    if raw is None:
+        raise KeyError("There are no tasks in the broker for UUID: " + uuid)
+    request_task = json.loads(raw)
+    try:
+        task_id = request_task[type_]
+    except KeyError as error:
+        raise KeyError("There are no tasks in the broker for type: " + type_) from error
+    return task_id
