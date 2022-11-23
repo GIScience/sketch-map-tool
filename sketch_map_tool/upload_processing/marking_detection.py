e94949c0fbffd1197b240a5e74cc109f84fa87dd
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2022-11-23 13:29:30 +0100

    Add contrast enhancement

--- sketch_map_tool/upload_processing/marking_detection.py
+++ sketch_map_tool/upload_processing/marking_detection.py
@@ -4,10 +4,24 @@ Functions to process images of sketch maps and detect markings on them
 """
 import numpy as np
 from numpy.typing import NDArray
+from PIL import ImageEnhance, ImageChops, Image
 import cv2
 
 
-def detect_markings(img_base_path, img_markings_path, threshold_bgr=0.5, threshold_img_diff=100):
+def enhance_contrast(img: NDArray, factor: float = 2.0) -> NDArray:
+    """
+    Enhance the contrast of a given image
+
+    :param img: Image of which the contrast should be enhanced
+    :param factor: Factor for the contrast enhancement
+    :return: Image with enhanced contrast
+    """
+    input_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    result = ImageEnhance.Contrast(input_img).enhance(factor)
+    return cv2.cvtColor(np.array(result), cv2.COLOR_RGB2BGR)
+
+
+def detect_markings(img_base, img_markings, threshold_bgr=0.5, threshold_img_diff=100):
     # threshold_bgr = 0.5  # 50%, i.e. 255*50% = 127
 
     threshold_bgr_abs = threshold_bgr * 255
@@ -21,11 +35,10 @@ def detect_markings(img_base_path, img_markings_path, threshold_bgr=0.5, thresho
               "pink": (255, 0, 255)
               }
 
-    img_base = cv2.imread(img_base_path)
     img_base_height, img_base_width, _ = img_base.shape
-    img_markings = cv2.imread(img_markings_path)
     img_markings = cv2.resize(img_markings, (img_base_width, img_base_height))
-    img_diff = cv2.absdiff(img_base, img_markings)
+    img_markings_contrast = enhance_contrast(img_markings)
+    img_diff = cv2.absdiff(img_base, img_markings_contrast)
 
     img_diff_gray = cv2.cvtColor(img_diff, cv2.COLOR_BGR2GRAY)
     mask_markings = img_diff_gray > threshold_img_diff
@@ -47,10 +60,10 @@ def detect_markings(img_base_path, img_markings_path, threshold_bgr=0.5, thresho
     cv2.imwrite("output/markings_all_multicolor.png", markings_multicolor)
     for i in range(len(single_color_markings)):
         single_color_markings[i][1] = reduce_holes(reduce_noise(single_color_markings[i][1]))
+    return single_color_markings
 
 
-
-def reduce_noise(img: NDArray, factor=4):
+def reduce_noise(img: NDArray, factor=2):
     """
     Reduce the noise, i.e. artifacts, in an image containing markings
 
@@ -62,7 +75,7 @@ def reduce_noise(img: NDArray, factor=4):
     return cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((factor, factor), np.uint8))
 
 
-def reduce_holes(img: NDArray, factor=4):
+def reduce_holes(img: NDArray, factor=5):
     """
     Reduce the holes in markings on a given image
 
