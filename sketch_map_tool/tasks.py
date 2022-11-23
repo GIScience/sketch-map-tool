1d4322877e584943650318f2b477979f33793da7
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-23 15:33:23 +0100

    feat: get map image for uploaded sketch map

--- sketch_map_tool/tasks.py
+++ sketch_map_tool/tasks.py
@@ -1,18 +1,19 @@
+from functools import reduce
 from io import BytesIO
 from typing import Union
-from functools import reduce
 
 from celery.result import AsyncResult
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
 from sketch_map_tool import celery_app as celery
+from sketch_map_tool.data_store.client import get_task_id
 from sketch_map_tool.map_generation import generate_pdf as generate_map_pdf
 from sketch_map_tool.map_generation.qr_code import qr_code
 from sketch_map_tool.models import Bbox, File, PaperFormat, Size
 from sketch_map_tool.oqt_analyses import generate_pdf as generate_report_pdf
 from sketch_map_tool.oqt_analyses import get_report
-from sketch_map_tool.upload_processing import qr_code_reader, map_cutter
+from sketch_map_tool.upload_processing import qr_code_reader
 from sketch_map_tool.wms import client as wms_client
 
 
@@ -36,13 +37,13 @@ def generate_sketch_map(
         size,
         scale,
     )
-    map_pdf, _ = generate_map_pdf(
+    map_pdf, map_img = generate_map_pdf(
         map_image,
         qr_code_,
         format_,
         scale,
     )
-    return map_pdf
+    return map_pdf, map_img
 
 
 @celery.task(bind=True)
@@ -63,11 +64,15 @@ def generate_digitized_results(self, files: list[File]) -> Union[BytesIO, AsyncR
     """Generate first raster data, then vector data and finally a QGIS project"""
     args = [qr_code_reader.read(file.image) for file in files]
 
-    # All uploaded sketch maps should have the same uuid
-    uuid = reduce(lambda a, b: a if a == b else None, [arg["uuid"] for arg in args]) 
+    # all uploaded sketch maps should have the same uuid
+    uuid = reduce(lambda a, b: a if a == b else None, [arg["uuid"] for arg in args])
     if uuid is None:
         raise ValueError  # TODO
-    
+
+    # get original map image
+    task_id = get_task_id(uuid, "sketch-map")
+    map_img = generate_sketch_map.AsyncResult(task_id).get()[1]
+
     bytes_buffer = BytesIO()
     canv = canvas.Canvas(bytes_buffer, pagesize=A4)
     canv.drawString(100, 100, "Digitized Results")
