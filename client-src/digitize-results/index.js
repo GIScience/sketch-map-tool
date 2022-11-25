e429451c6bebd8cf2f70a2e5b139c2ddc422ead0
Michael Auer <michael.auer@heigit.org>
2022-11-25 18:11:00 +0100

    add messages and handle polling of digitize results

--- client-src/digitize-results/index.js
+++ client-src/digitize-results/index.js
@@ -2,5 +2,14 @@ import { getUUIDFromURL, poll, handleMainMessage } from "../shared";
 
 const digitizedResultsUrl = `/api/status/${getUUIDFromURL()}/digitized-data`;
 
-// TODO handle all three results
-poll(digitizedResultsUrl, "vector-data").then(handleMainMessage);
+const rasterResultsUrl = `/api/status/${getUUIDFromURL()}/raster-data`;
+
+const qgisResultsUrl = `/api/status/${getUUIDFromURL()}/qgis-data`;
+
+// TODO handle all three results, decide one two or three results
+
+Promise.all([
+    poll(rasterResultsUrl, "raster-data"),
+    poll(digitizedResultsUrl, "vector-data"),
+    poll(qgisResultsUrl, "qgis-data"),
+]).then(handleMainMessage);
