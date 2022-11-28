68c95622c92f41719ebc7787c8af4655cf80a9d4
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-28 16:59:49 +0100

    feat: add vectorization
    raster to vector conversion using gdal.polygonize

---
+++ sketch_map_tool/upload_processing/vectorization.py
@@ -0,0 +1,37 @@
+from io import BytesIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+
+from osgeo import gdal, ogr, osr
+
+
+def polygonize(geotiff: BytesIO, layer_name: str) -> BytesIO:
+    """Produces a polygon feature layer (GeoJSON) from a raster (GeoTIFF)."""
+    gdal.UseExceptions()
+    ogr.UseExceptions()
+
+    # open geotiff
+    infile = NamedTemporaryFile(suffix=".geotiff")
+    with open(infile.name, "wb") as f:
+        f.write(geotiff.getbuffer())
+    src_ds = gdal.Open(infile.name)
+    srs = osr.SpatialReference(wkt=src_ds.GetProjection())
+
+    with TemporaryDirectory() as tmpdirname:
+        outfile_name = Path(tmpdirname) / "out.geojson"
+
+        # open geojson
+        driver = ogr.GetDriverByName("GeoJSON")
+        dst_ds = driver.CreateDataSource(str(outfile_name))
+
+        dst_layer = dst_ds.CreateLayer(layer_name, srs=srs)
+        # src_layer.CreateField(ogr.FieldDefn("color", ogr.OFTString))
+        src_band = src_ds.GetRasterBand(1)
+
+        # (srcBand, maskBand, outLayer, iPixValField)
+        gdal.Polygonize(src_band, None, dst_layer, -1)
+
+        src_ds = None  # close dataset
+        dst_ds = None  # close dataset
+        with open(outfile_name, "rb") as f:
+            return BytesIO(f.read())
---
+++ tests/unit/upload_processing/test_vectorization.py
@@ -0,0 +1,9 @@
+# import sqlite3
+from io import BytesIO
+
+from sketch_map_tool.upload_processing.vectorization import polygonize
+
+
+def test_polygonize(map_frame_markings_detected_buffer):
+    geojson_buffer = polygonize(map_frame_markings_detected_buffer, "red")
+    assert isinstance(geojson_buffer, BytesIO)
