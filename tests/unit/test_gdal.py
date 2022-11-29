7d63d91c5bcb72d9f334bac4d773799d10de4929
Johannes Visintini <johannes.visintini@heigit.org>
2022-11-29 12:29:29 +0100

    add test for gqis imports

--- tests/unit/test_gdal.py
+++
@@ -1,5 +0,0 @@
-def test_gdal_array():
-    try:
-        from osgeo import gdal_array  # noqa
-    except ImportError:
-        assert False
---
+++ tests/unit/test_imports.py
@@ -0,0 +1,14 @@
+def test_gdal_array():
+    try:
+        from osgeo import gdal_array  # noqa
+    except ImportError:
+        assert False
+
+
+def test_qgis():
+    try:
+        import processing  # noqa
+        import qgis.analysis  # noqa
+        import qgis.core  # noqa
+    except ImportError:
+        assert False
