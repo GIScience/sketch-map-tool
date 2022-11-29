5a509ba915ee6aea58315c1b765d17c4b239226a
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-29 13:40:11 +0100

    tests: refine fixtures and add integration tests

--- tests/unit/upload_processing/test_polygonize.py
+++ tests/unit/upload_processing/test_polygonize.py
@@ -4,6 +4,6 @@ from io import BytesIO
 from sketch_map_tool.upload_processing import polygonize
 
 
-def test_polygonize(map_frame_markings_detected_buffer):
-    geojson_buffer = polygonize(map_frame_markings_detected_buffer, "red")
+def test_polygonize(sketch_map_frame_markings_detected_buffer):
+    geojson_buffer = polygonize(sketch_map_frame_markings_detected_buffer, "red")
     assert isinstance(geojson_buffer, BytesIO)
