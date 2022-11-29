ad2c15ba3d695668d61f6fcc1529f60b0cccaede
Matthias Schaub <matthias.schaub@heigit.org>
2022-11-29 10:10:00 +0100

    feat(celery): refine celery workflow
    refine celery workflow by chaining and grouping tasks together

--- sketch_map_tool/tasks.py
+++ sketch_map_tool/tasks.py
@@ -2,12 +2,13 @@ from io import BytesIO
 
 import cv2
 import numpy as np
-from celery import group
+from celery import chain, group
 from celery.result import AsyncResult
 from numpy.typing import NDArray
 
 from sketch_map_tool import celery_app as celery
 from sketch_map_tool import map_generation, upload_processing
+from sketch_map_tool.definitions import COLORS
 from sketch_map_tool.models import Bbox, PaperFormat, Size
 from sketch_map_tool.oqt_analyses import generate_pdf as generate_report_pdf
 from sketch_map_tool.oqt_analyses import get_report
@@ -54,33 +55,53 @@ def generate_quality_report(bbox: Bbox) -> BytesIO | AsyncResult:
 
 
 # GENERATE DIGITIZED RESULTS
+# fmt: off
 def generate_digitized_results(files):
-    args = upload_processing.qr_code_reader(buffer_to_array(files[0]))
+    args = upload_processing.read_qr_code(t_buffer_to_array(files[0]))
     uuid = args["uuid"]
-    # bbox = args["bbox"]
+    bbox = args["bbox"]
 
     map_frame_buffer = celery.AsyncResult(uuid).get()[1]
-    map_frame = buffer_to_array(map_frame_buffer)
+    map_frame = t_buffer_to_array(map_frame_buffer)
 
-    workflow = (
-        group(
-            [
-                (
-                    buffer_to_array.s(sketch_map)
-                    | clip.s(map_frame)
-                    | detect.s(map_frame)
+    # Design Celery Workflow
+    #
+    # https://docs.celeryq.dev/en/stable/userguide/canvas.html
+    #
+    # t_    -> task
+    # c_    -> chain of tasks (sequential)
+    # group -> group of tasks (parallel)
+    #
+    def c_digitize(color: str) -> chain:
+        """Digitize one color of a Sketch Map."""
+        return (
+                t_detect.s(map_frame, color)
+                | t_georeference.s(bbox)
+                | t_polygonize.s(color)
                 )
-                for sketch_map in files
-            ]
+
+    def c_process(sketch_map: BytesIO) -> chain:
+        """Process a Sketch Map."""
+        return (
+            t_buffer_to_array.s(sketch_map)
+            | t_clip.s(map_frame)
+            | group([c_digitize(c) for c in COLORS])
+            | t_merge.s()
         )
-        | aggregate.s()
-    )
-    result = workflow.apply_async()
+
+    def c_workflow(files) -> chain:
+        """Start processing workflow for each file."""
+        return (
+                group([c_process(f) for f in files])
+                | t_zip.s()
+                )
+    result = c_workflow(files).apply_async()
     return result.id
+    # fmt: on
 
 
 @celery.task()
-def buffer_to_array(buffer: BytesIO) -> NDArray:
+def t_buffer_to_array(buffer: BytesIO) -> AsyncResult | NDArray:
     buffer.seek(0)
     return cv2.imdecode(
         np.fromstring(buffer.read(), dtype="uint8"), cv2.IMREAD_UNCHANGED
@@ -88,20 +109,32 @@ def buffer_to_array(buffer: BytesIO) -> NDArray:
 
 
 @celery.task()
-def clip(sketch_map: NDArray, map_frame: NDArray) -> AsyncResult | NDArray:
+def t_clip(sketch_map: NDArray, map_frame: NDArray) -> AsyncResult | NDArray:
     return upload_processing.clip(sketch_map, map_frame)
 
 
 @celery.task()
-def detect(sketch_map_frame: NDArray, map_frame: NDArray) -> AsyncResult | NDArray:
-    return upload_processing.detect_markings(map_frame, sketch_map_frame)
+def t_detect(
+    sketch_map_frame: NDArray, map_frame: NDArray, color
+) -> AsyncResult | NDArray:
+    return upload_processing.detect_markings(map_frame, sketch_map_frame, color)
 
 
 @celery.task()
-def img_to_geotiff(sketch_map_frame: NDArray, bbox: Bbox) -> AsyncResult | BytesIO:
-    return upload_processing.img_to_geotiff(sketch_map_frame, bbox)
+def t_georeference(sketch_map_frame: NDArray, bbox: Bbox) -> AsyncResult | BytesIO:
+    return upload_processing.georeference(sketch_map_frame, bbox)
 
 
 @celery.task()
-def aggregate(*args, **kwargs) -> str:
-    return BytesIO(b"Mock")
+def t_polygonize(geotiff: BytesIO, layer_name: str) -> AsyncResult | BytesIO:
+    return upload_processing.polygonize(geotiff, layer_name)
+
+
+@celery.task()
+def t_merge(geojsons: list) -> AsyncResult | BytesIO:
+    return upload_processing.merge(geojsons)
+
+
+@celery.task()
+def t_zip(args) -> AsyncResult | BytesIO:
+    return args[0]
