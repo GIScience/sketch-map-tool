8682b44331221a09baa92c3a416c6ccf47ccefaf
Michael Auer <michael.auer@heigit.org>
2022-12-01 16:28:06 +0100

    remove qgis result

--- client-src/digitize-results/index.js
+++ client-src/digitize-results/index.js
@@ -1,13 +1,9 @@
 import { getUUIDFromURL, poll, handleMainMessage } from "../shared";
 
 const vectorResultsUrl = `/api/status/${getUUIDFromURL()}/vector-results`;
-
 const rasterResultsUrl = `/api/status/${getUUIDFromURL()}/raster-results`;
-
-// const qgisResultsUrl = `/api/status/${getUUIDFromURL()}/qgis-data`;
 
 Promise.all([
     poll(rasterResultsUrl, "raster-data"),
     poll(vectorResultsUrl, "vector-data"),
-    // poll(qgisResultsUrl, "qgis-data"),
 ]).then(handleMainMessage);
