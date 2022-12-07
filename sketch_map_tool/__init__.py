c7982b502cb50214add562082de3c0a033647a88
Johannes Visintini <johannes.visintini@heigit.org>
2022-12-07 16:51:21 +0100

    Optimize celery and resource settings

--- sketch_map_tool/__init__.py
+++ sketch_map_tool/__init__.py
@@ -14,7 +14,9 @@ def make_flask() -> Flask:
             "broker_url": get_config_value("broker-url"),
             "result_backend": get_config_value("result-backend"),
             "task_serializer": "pickle",
-            "result_serializer": "pickle",
+            "result_serializer": "json",
+            "result_chord_join_timeout": 10.0,  # default: 3.0 seconds
+            "result_chord_retry_interval": 3.0,  # default: 1.0 seconds
             "accept_content": ["application/json", "application/x-python-serialize"],
         }
     )
