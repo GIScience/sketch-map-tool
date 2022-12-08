a817b327d42013852e14889d5dc62312b9fc6470
Johannes Visintini <johannes.visintini@heigit.org>
2022-12-08 15:06:14 +0100

    map generation: remove old scale

--- sketch_map_tool/map_generation/generate_pdf.py
+++ sketch_map_tool/map_generation/generate_pdf.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib_scalebar.scalebar import ScaleBar
 from PIL import Image as PILImage
 from reportlab.graphics import renderPDF
-from reportlab.graphics.shapes import Drawing, Rect, String
+from reportlab.graphics.shapes import Drawing
 from reportlab.lib.pagesizes import landscape
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.units import cm
@@ -223,14 +223,6 @@ def draw_right_column(
     qr_size = min(width, height) - margin
     qr_code = resize_rlg_by_width(qr_code, qr_size)
 
-    # Add scale:
-    scale = get_scale(
-        scale,
-        width_max=width - margin,
-        scale_height=format_.scale_height * cm,
-        font_size=em,
-    )
-
     # fills up the remaining space, placed between the TOP and the BOTTOM aligned elements
     space_filler = Spacer(width, 0)  # height will be filled after list creation
     # order all elements in column
@@ -240,8 +232,6 @@ def draw_right_column(
         heigit_logo,
         space_filler,
         compass,
-        Spacer(width, 2 * em),
-        scale,
         Spacer(width, 2 * em),
         p_copyright,
         Spacer(width, 2 * em),
@@ -403,74 +393,6 @@ def get_compass(size: float) -> Drawing:
     compass = svg2rlg(PDF_RESOURCES_PATH / "north.svg")
     compass = resize_rlg_by_width(compass, size)
     return compass
-
-
-def get_scale(
-    scale_value: float, width_max: float, scale_height: float, font_size: float
-) -> tuple[float, tuple[str, str]]:
-    """Get scale length [cm] and scale text.
-
-    scale_value: Scale denominator
-    width_max: Maximal width of the scale bar [cm]
-    scale_height: Height of the returned scale
-
-    E.g.
-    (scale bar) 1 cm = 11545.36 cm (scale denominator)
-    (scale bar) ? cm = 50000 cm    (factor)
-    """
-    for factor in (
-        1000000,  # 10 km
-        500000,
-        200000,
-        100000,  # 1 km
-        50000,
-        20000,
-        10000,  # 100 m
-        5000,
-        1000,  # 10 m
-    ):
-        scale_length = factor / scale_value
-        if scale_length <= width_max:
-            # Two parts of the black and white scale bar
-            if factor >= 100000:
-                # In kilometer
-                scale_text = (
-                    str(int((factor / 100000) / 2)) + "km",
-                    str(int(factor / 100000)) + "km",
-                )
-            else:
-                # In meter
-                scale_text = (
-                    str(int((factor / 100) / 2)) + "m",
-                    str(int(factor / 100)) + "m",
-                )
-            break
-    scale = Drawing(width_max, scale_height + font_size * 1.25)
-    scale.add(
-        Rect(
-            0,
-            scale.height - scale_height,
-            scale_length / 2,
-            scale_height,
-            fillColor="black",
-        )
-    )
-    scale.add(
-        Rect(
-            scale_length / 2,
-            scale.height - scale_height,
-            scale_length / 2,
-            scale_height,
-            fillColor="white",
-        )
-    )
-    scale.add(
-        String(scale_length / 2, 0, scale_text[0], fontSize=font_size, textAnchor="end")
-    )
-    scale.add(
-        String(scale_length, 0, scale_text[1], fontSize=font_size, textAnchor="end")
-    )
-    return scale
 
 
 def pdf_page_to_img(pdf: BytesIO, page_id=0) -> BytesIO:
