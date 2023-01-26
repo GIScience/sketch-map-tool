52213f4d05e004b08d658e32f54a059bc0e30dc2
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-01-26 18:40:11 +0100

    Update locustfile.py: Download result files and log resource use
    Result files of all kinds are stored to allow for manual inspection whether they look as expected. The resource use, i.e. the percentage of RAM being used, is now logged in a CSV file to enable analyses afterwards

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,11 +1,14 @@
 import logging
 import os
+import time
+from threading import Thread
 from time import sleep
 from uuid import UUID
 
 from locust import HttpUser, constant, run_single_user, task
 
 logging.basicConfig()
+result_index = 0
 
 
 def validate_uuid(uuid: str):
@@ -15,14 +18,25 @@ def validate_uuid(uuid: str):
         raise (ValueError('"{}" is not valid a UUID!'.format(uuid)))
 
 
+def monitor_resource_use(wait_time_sec: int = 1):
+    while True:
+        os.system(
+            'docker stats -a --no-stream --format "{{.Name}},{{.MemPerc}}" >> resource_log.csv'
+        )
+        time.sleep(wait_time_sec)
+
+
 class WorkflowCycle(HttpUser):
     host = os.getenv("LOCUST_FLASK_HOST", default="http://localhost:8081")
     wait_time = constant(int(os.getenv("LOCUST_WAIT_TIME", default=6)))
     api_status_wait_time = int(os.getenv("LOCUST_API_STATUS_WAIT_TIME", default=5))
     api_status_retries = int(os.getenv("LOCUST_API_STATUS_RETRIES", default=30))
+    thread_resource_monitoring = Thread(target=monitor_resource_use)
+    thread_resource_monitoring.start()
 
     @task
     def create(self):
+        global result_index
         payload = {
             "format": "A4",
             "orientation": "landscape",
@@ -34,12 +48,26 @@ class WorkflowCycle(HttpUser):
         create_post = self.client.post("/create/results", data=payload)
         create_uuid = create_post.url.split("/")[-1]
         validate_uuid(create_uuid)
-        download_url = self.status_loop(create_uuid, "sketch-map")
-        request_name = "/api/download/[uuid]/sketch-map"
-        self.client.get(download_url, name=request_name)
+
+        if not os.path.exists("results"):
+            os.mkdir("results")
+        for result_type in ("sketch-map", "quality-report"):
+            download_url = self.status_loop(create_uuid, result_type)
+            request_name = f"/api/download/[uuid]/{result_type}"
+            result = self.client.get(download_url, name=request_name)
+
+            if result.status_code == 200:
+                if not os.path.exists(f"results/{result_type}"):
+                    os.mkdir(f"results/{result_type}")
+                with open(f"results/{result_type}/file_{result_index}.pdf", "wb") as f:
+                    f.write(result.content)
+            else:
+                raise ValueError(f"Unexpected status code: '{result.status_code}'")
+            result_index += 1
 
     @task
     def digitize(self):
+        global result_index
         num_files = int(os.getenv("LOCUST_NUM_FILES", default=1))
         map_filename = os.getenv("LOCUST_MAP_FILENAME", default="sketch-map.png")
         files = [("file", open(map_filename, "rb")) for _ in range(num_files)]
@@ -49,7 +77,16 @@ class WorkflowCycle(HttpUser):
         for result_type in ("raster-results", "vector-results"):
             download_url = self.status_loop(digitize_uuid, result_type)
             request_name = "/api/download/[uuid]/{}".format(result_type)
-            self.client.get(download_url, name=request_name)
+            result = self.client.get(download_url, name=request_name)
+
+            if result.status_code == 200:
+                if not os.path.exists(f"results/{result_type}"):
+                    os.mkdir(f"results/{result_type}")
+                with open(f"results/{result_type}/file_{result_index}", "wb") as f:
+                    f.write(result.content)
+            else:
+                raise ValueError(f"Unexpected status code: '{result.status_code}'")
+            result_index += 1
 
     def status_loop(self, uuid: str, request_type: str):
         for _ in range(self.api_status_retries):
