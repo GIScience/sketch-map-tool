9b45d26060d9ba8696ec9f7cb1d51a176476a3d9
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-02-28 15:34:31 +0100

    Remove unused module geopackage and associated test

--- sketch_map_tool/upload_processing/__init__.py
+++ sketch_map_tool/upload_processing/__init__.py
@@ -2,14 +2,12 @@ from .clean import clean
 from .clip import clip
 from .detect_markings import detect_markings, prepare_img_for_markings
 from .enrich import enrich
-from .geopackage import geopackage
 from .georeference import georeference
 from .merge import merge
 from .polygonize import polygonize
 from .qr_code_reader import read as read_qr_code
 
 __all__ = (
-    "geopackage",
     "enrich",
     "clean",
     "clip",
