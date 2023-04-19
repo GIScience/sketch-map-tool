7c7bf9df5c28045292a72b139515cf8c0ea8f067
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-04-19 17:13:49 +0200

    Apply center point from URL params

--- client-src/create/index.js
+++ client-src/create/index.js
@@ -7,7 +7,18 @@ import "./create.css";
 import { createMap, addPrintLayoutControl, addGeocoderControl } from "./map.js";
 import { bindFormToPrintLayoutControl } from "./form.js";
 
+// Retrieve potentially given map center from URL (e.g. from a bookmarked selection)
+const searchParams = new URLSearchParams(window.location.search);
+const centerArg = searchParams.get("center");
+
 const map = createMap("map", [8.68, 49.41], 15);
+
+if (centerArg != null) {
+    const centerCoords = centerArg.split(",");
+    const center = [parseFloat(centerCoords[0]), parseFloat(centerCoords[1])];
+    map.getView().setCenter(center);
+}
+
 const printLayoutControl = addPrintLayoutControl(map);
 bindFormToPrintLayoutControl(printLayoutControl);
 addGeocoderControl(map);
