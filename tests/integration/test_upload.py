716e1a61aa87a3624e5ae5de0e93699dc36e01af
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-05-16 11:27:10 +0200

    Update tests\integration\test_upload.py: Add test case for allowed number of uploads

--- tests/integration/test_upload.py
+++ tests/integration/test_upload.py
@@ -1,12 +1,11 @@
 import os
-from io import BytesIO
 
 import pytest
 
-from sketch_map_tool.exceptions import UploadLimitsExceededError
+from sketch_map_tool.exceptions import FileNotFoundError_, UploadLimitsExceededError
 
 
-def test_too_many_uploads(flask_client):
+def test_too_many_uploads(flask_client, sketch_map_markings_buffer_1):
     old_max_nr = os.getenv("SMT-MAX-NR-SIM-UPLOADS")
     os.environ["SMT-MAX-NR-SIM-UPLOADS"] = "2"
     with pytest.raises(UploadLimitsExceededError):
@@ -14,9 +13,32 @@ def test_too_many_uploads(flask_client):
             "/digitize/results",
             data=dict(
                 file=[
-                    (BytesIO(b"File1"), "file1.png"),
-                    (BytesIO(b"File2"), "file2.png"),
-                    (BytesIO(b"File3"), "file3.png"),
+                    (sketch_map_markings_buffer_1, "file1.png"),
+                    (sketch_map_markings_buffer_1, "file2.png"),
+                    (sketch_map_markings_buffer_1, "file3.png"),
+                ],
+            ),
+            follow_redirects=True,
+        )
+    if old_max_nr is None:
+        os.unsetenv("SMT-MAX-NR-SIM-UPLOADS")
+    else:
+        os.environ["SMT-MAX-NR-SIM-UPLOADS"] = old_max_nr
+
+
+def test_allowed_nr_of_uploads(flask_client, sketch_map_markings_buffer_1):
+    # Successful run requires that a sketch map has been generated on the instance beforehand
+    old_max_nr = os.getenv("SMT-MAX-NR-SIM-UPLOADS")
+    os.environ["SMT-MAX-NR-SIM-UPLOADS"] = "2"
+    with pytest.raises(
+        FileNotFoundError_
+    ):  # uuid of test image not in database, but the exception shows that the uploads have been accepted and processed
+        flask_client.post(
+            "/digitize/results",
+            data=dict(
+                file=[
+                    (sketch_map_markings_buffer_1, "file1.png"),
+                    (sketch_map_markings_buffer_1, "file2.png"),
                 ],
             ),
             follow_redirects=True,
