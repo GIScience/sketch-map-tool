b078b0a851a3a3b9c45ad3f079a2c3e39227c47c
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-03 18:12:16 +0200

    Update tests\integration\test_upload.py: Mock env. variables in test

--- tests/integration/test_upload.py
+++ tests/integration/test_upload.py
@@ -1,13 +1,13 @@
 import os
+from unittest import mock
 
 import pytest
 
 from sketch_map_tool.exceptions import FileNotFoundError_, UploadLimitsExceededError
 
 
+@mock.patch.dict(os.environ, {"SMT-MAX-NR-SIM-UPLOADS": "2"})
 def test_too_many_uploads(flask_client, sketch_map_markings_buffer_1):
-    old_max_nr = os.getenv("SMT-MAX-NR-SIM-UPLOADS")
-    os.environ["SMT-MAX-NR-SIM-UPLOADS"] = "2"
     with pytest.raises(UploadLimitsExceededError):
         flask_client.post(
             "/digitize/results",
@@ -20,16 +20,11 @@ def test_too_many_uploads(flask_client, sketch_map_markings_buffer_1):
             ),
             follow_redirects=True,
         )
-    if old_max_nr is None:
-        os.unsetenv("SMT-MAX-NR-SIM-UPLOADS")
-    else:
-        os.environ["SMT-MAX-NR-SIM-UPLOADS"] = old_max_nr
 
 
+@mock.patch.dict(os.environ, {"SMT-MAX-NR-SIM-UPLOADS": "2"})
 def test_allowed_nr_of_uploads(flask_client, sketch_map_markings_buffer_1):
     # Successful run requires that a sketch map has been generated on the instance beforehand
-    old_max_nr = os.getenv("SMT-MAX-NR-SIM-UPLOADS")
-    os.environ["SMT-MAX-NR-SIM-UPLOADS"] = "2"
     with pytest.raises(
         FileNotFoundError_
     ):  # uuid of test image not in database, but the exception shows that the uploads have been accepted and processed
@@ -43,7 +38,3 @@ def test_allowed_nr_of_uploads(flask_client, sketch_map_markings_buffer_1):
             ),
             follow_redirects=True,
         )
-    if old_max_nr is None:
-        os.unsetenv("SMT-MAX-NR-SIM-UPLOADS")
-    else:
-        os.environ["SMT-MAX-NR-SIM-UPLOADS"] = old_max_nr
