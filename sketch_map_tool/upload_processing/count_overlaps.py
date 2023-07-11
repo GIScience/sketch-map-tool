1945d4b5fd9189287a538d4cb91428a2c9e2378f
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-11 14:35:29 +0200

    Update count_overlaps.py: Add GeoJSON and QGIS project to ZIP

--- sketch_map_tool/upload_processing/count_overlaps.py
+++ sketch_map_tool/upload_processing/count_overlaps.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from tempfile import NamedTemporaryFile
 from qgis.core import (QgsProject, QgsVectorLayer, QgsCoordinateReferenceSystem, QgsApplication,
                        QgsProcessingFeatureSourceDefinition, QgsProcessingFeedback)
+from zipfile import ZipFile
 
 
 def create_qgis_project(markings: BytesIO):
@@ -16,5 +17,9 @@ def create_qgis_project(markings: BytesIO):
     project.setCrs(reference_system)
     outfile = NamedTemporaryFile(suffix=".qgs")
     project.write(outfile.name)
-    with open(outfile.name, "rb") as f:
-        return BytesIO(f.read())
+    buffer = BytesIO()
+    with ZipFile(buffer, "w") as zip_file, open(infile.name, "rb") as f_markings, open(outfile.name, "rb") as f_qgis:
+        zip_file.writestr(infile.name.replace("tmp/", "./"), f_markings.read())
+        zip_file.writestr(f"project.qgs", f_qgis.read())
+    buffer.seek(0)
+    return buffer
