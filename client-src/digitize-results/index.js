77f2294a8710e42edfd576739a32b6ae3cb4524c
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-11 11:35:14 +0200

    Add UI for downloading further analyses' results

--- client-src/digitize-results/index.js
+++ client-src/digitize-results/index.js
@@ -2,8 +2,10 @@ import { getUUIDFromURL, poll, handleMainMessage } from "../shared";
 
 const vectorResultsUrl = `/api/status/${getUUIDFromURL()}/vector-results`;
 const rasterResultsUrl = `/api/status/${getUUIDFromURL()}/raster-results`;
+const qgisResultsUrl = `/api/status/${getUUIDFromURL()}/qgis-data`;
 
 Promise.all([
     poll(rasterResultsUrl, "raster-data"),
     poll(vectorResultsUrl, "vector-data"),
+    poll(qgisResultsUrl, "qgis-data"),
 ]).then(handleMainMessage);
