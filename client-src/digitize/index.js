e6a1edf12f072450781886ce4676de2efabdcf96
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-11 16:14:10 +0200

    Revert "Update client-src\digitize\index.js: Fix #262"
    This reverts commit bb0673a33ccd94ce9d7fe3f54adc35f963781125.

--- client-src/digitize/index.js
+++ client-src/digitize/index.js
@@ -29,17 +29,3 @@ document.forms.namedItem("upload").onsubmit = () => {
     setDisabled("submitBtn", true);
     setIsBusy("submitBtn", true);
 };
-
-// In case the page is newly loaded, reset the upload button, the spinner, and the upload field.
-// This prevents unexpected behaviour when clicking the back button after an upload.
-window.addEventListener("pageshow", () => {
-    setDisabled("submitBtn", true);
-    setIsBusy("submitBtn", false);
-
-    // Remove all previously selected files. The used file selector (filebokz) seems not to
-    // provide a working solution to remove all selected files at once.
-    const removeButtons = document.getElementsByClassName("remove");
-    for (let i = removeButtons.length - 1; i >= 0; i--) {
-        removeButtons[i].click();
-    }
-});
