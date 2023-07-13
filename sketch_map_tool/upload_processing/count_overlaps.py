b7fac83a686960b70309fc2349f44f562ac61aa4
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-13 18:04:54 +0200

    Refactor count_overlaps.py and improve documentation

--- sketch_map_tool/upload_processing/count_overlaps.py
+++ sketch_map_tool/upload_processing/count_overlaps.py
@@ -20,23 +20,30 @@ from qgis.core import (
 )
 
 
-def create_qgis_project(markings: BytesIO):
+def create_qgis_project(markings: BytesIO) -> Tuple[BytesIO, BytesIO]:
+    """
+    Create a QGIS project with the detected markings as layer, run a QGIS script to count the overlapping markings
+    (from different sketch maps) per colour, resulting in an additional layer in the project.
+
+    :param markings: GeoJSON with the detected markings.
+    :return: (ZIP file containing the QGIS project and both layers as GeoJSONs, GeoJSON of the overlap counts).
+    """
     project = QgsProject()
     project.setTitle("Sketch Map Tool Results")
+    reference_system = QgsCoordinateReferenceSystem("EPSG:4326")
+    project.setCrs(reference_system)
+
+    # Add detected markings as layer
     infile = NamedTemporaryFile(prefix="markings_", suffix=".geojson")
     with open(infile.name, "wb") as f:
         f.write(markings.read())
     layer = QgsVectorLayer(infile.name, "Markings", "ogr")
     project.addMapLayer(layer)
-    reference_system = QgsCoordinateReferenceSystem("EPSG:4326")
-    project.setCrs(reference_system)
-    result_file = NamedTemporaryFile(prefix="overlap_counts_", suffix=".geojson")
-    Processing.initialize()
 
     # Add custom QGIS scripts to processing toolbox:
+    Processing.initialize()
     scripts_path = pathlib.Path(__file__).parent.resolve() / "qgis_scripts"
     qgis_scripts_path = ScriptUtils.defaultScriptsFolder()
-
     added_scripts = False
     for filename in os.listdir(scripts_path):
         if filename not in os.listdir(qgis_scripts_path):
@@ -49,21 +56,26 @@ def create_qgis_project(markings: BytesIO):
     if added_scripts:
         QgsApplication.processingRegistry().providerById("script").refreshAlgorithms()
 
+    # Run the overlapping count script
+    result_file = NamedTemporaryFile(prefix="overlap_counts_", suffix=".geojson")
     processing.run(
         "script:split_count_merge",
         {
             "inlayer": infile.name,
             "OutputLayer": result_file.name,
             "uniqueidfield": "color",
-            # 'OUTPUT': 'memory:'
         },
     )
+
     layer_result = QgsVectorLayer(result_file.name, "Overlap Counts", "ogr")
     project.addMapLayer(layer_result)
+
     outfile = NamedTemporaryFile(suffix=".qgs")
     project.write(outfile.name)
     buffer_project = BytesIO()
-    buffer_overlaps = BytesIO()
+    buffer_overlaps = (
+        BytesIO()
+    )  # Both included in the project ZIP and returned separately for the heatmap generation
     with (
         ZipFile(buffer_project, "w") as zip_file,
         open(infile.name, "rb") as f_markings,
@@ -84,15 +96,15 @@ def generate_heatmaps(
     geojson_path, lon_min, lat_min, lon_max, lat_max, bg_img_path
 ) -> List[Tuple[str, BytesIO]]:
     """
-    Create a heatmap covering the extent given in 'lon_min', ..., 'lat_max' arguments, based on the
-    'COUNT' data in the GeoJSON referred to with 'geojson_path'.
+    Create heatmaps covering the extent given in 'lon_min', ..., 'lat_max' arguments, based on the
+    'COUNT' data in the GeoJSON referred to with 'geojson_path' for each colour (as stored in the property 'color').
 
-    :param geojson_path: Path to a GeoJSON containing a 'COUNT' property for all features.
+    :param geojson_path: Path to a GeoJSON containing a 'COUNT' and a 'color' property for all features.
     :param lon_min: Longitude in web mercator of the lower left corner of the extent.
     :param lat_min: Latitude in web mercator of the lower left corner of the extent.
     :param lon_max: Longitude in web mercator of the upper right corner of the extent.
     :param lat_max: Latitude in web mercator of the upper right corner of the extent.
-    :param bg_img_path: Path to the map image shown as background to the heatmap.
+    :param bg_img_path: Path to the map image to be used as background for the heatmaps.
     :return: JPGs of the heatmaps for each colour of detected markings and the names of the
              corresponding colours.
     """
@@ -111,7 +123,10 @@ def generate_heatmaps(
         img = plt.imread(bg_img_path)
         ax.imshow(img, extent=[lon_min, lon_max, lat_min, lat_max])
 
+        # Plot heatmanp
         df_col.plot(column="COUNT", cmap="cividis", ax=ax)
+
+        # Add legend limited to integer values in the observed range of counts
         plt.colorbar(
             ax.get_children()[1],
             ax=ax,
