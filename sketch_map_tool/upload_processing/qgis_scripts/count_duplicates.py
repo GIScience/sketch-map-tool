d6a7ec9865840e4797695d05387835df62335407
mhhd2020 <74619995+mhhd2020@users.noreply.github.com>
2023-07-15 14:54:47 +0200

    Run autoformatters

--- sketch_map_tool/upload_processing/qgis_scripts/count_duplicates.py
+++ sketch_map_tool/upload_processing/qgis_scripts/count_duplicates.py
@@ -2,43 +2,44 @@
 Count duplicate, i.e. overlapping, features of input layer.
 """
 
-from PyQt5.QtCore import QCoreApplication
-from qgis.core import (QgsProcessing,
-                       QgsFeatureSink,
-                       QgsProcessingException,
-                       QgsProcessingAlgorithm,
-                       QgsProcessingParameterFeatureSource,
-                       QgsProcessingParameterFeatureSink,
-                       QgsProcessingParameterString)
 import processing
-
-from qgis.core import QgsField, QgsFeature
-from PyQt5.QtCore import QVariant
+from PyQt5.QtCore import QCoreApplication, QVariant
+from qgis.core import (
+    QgsFeature,
+    QgsFeatureSink,
+    QgsField,
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingException,
+    QgsProcessingParameterFeatureSink,
+    QgsProcessingParameterFeatureSource,
+    QgsProcessingParameterString,
+)
 
 
 # TODO: Refactor
 class CountDuplicates(QgsProcessingAlgorithm):
-    INPUT = 'INPUT'
-    OUTPUT = 'OUTPUT'
-    ID_FIELD = 'ID_FIELD'
+    INPUT = "INPUT"
+    OUTPUT = "OUTPUT"
+    ID_FIELD = "ID_FIELD"
 
     def tr(self, string):
-        return QCoreApplication.translate('Processing', string)
+        return QCoreApplication.translate("Processing", string)
 
     def createInstance(self):
         return CountDuplicates()
 
     def name(self):
-        return 'count_duplicates'
+        return "count_duplicates"
 
     def displayName(self):
-        return self.tr('Count duplicates')
+        return self.tr("Count duplicates")
 
     def group(self):
-        return self.tr('')
+        return self.tr("")
 
     def groupId(self):
-        return ''
+        return ""
 
     def shortHelpString(self):
         return self.tr("Count Duplicates in input layer.")
@@ -48,40 +49,35 @@ class CountDuplicates(QgsProcessingAlgorithm):
         self.addParameter(
             QgsProcessingParameterFeatureSource(
                 self.INPUT,
-                self.tr('Input layer'),
-                [QgsProcessing.TypeVectorAnyGeometry]
+                self.tr("Input layer"),
+                [QgsProcessing.TypeVectorAnyGeometry],
             )
         )
 
         # Add a feature sink in which to store our processed features (this usually takes the form of a
         # newly created vector layer)
         self.addParameter(
-            QgsProcessingParameterFeatureSink(
-                self.OUTPUT,
-                self.tr('Output layer')
-            )
+            QgsProcessingParameterFeatureSink(self.OUTPUT, self.tr("Output layer"))
         )
 
     def processAlgorithm(self, parameters, context, feedback):
         # Retrieve the feature source and sink
-        source = self.parameterAsSource(
-            parameters,
-            self.INPUT,
-            context
-        )
+        source = self.parameterAsSource(parameters, self.INPUT, context)
 
         if source is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+            raise QgsProcessingException(
+                self.invalidSourceError(parameters, self.INPUT)
+            )
 
         # Create the output layer
         out_fields = source.fields()
-        out_fields.append(QgsField("COUNT", QVariant.Int, '', 10, 0))
-        
+        out_fields.append(QgsField("COUNT", QVariant.Int, "", 10, 0))
+
         # Add id fields, used to TODO
         name_index = source.fields().indexOf("name")
         values = source.uniqueValues(name_index)
         for id_value in values:
-            out_fields.append(QgsField(str(id_value), QVariant.Int, '', 10, 0))
+            out_fields.append(QgsField(str(id_value), QVariant.Int, "", 10, 0))
         out_fields.remove(name_index)
         # TODO: Contents of out_fields now -> Can the initialisation be made more clearly?
 
@@ -92,9 +88,9 @@ class CountDuplicates(QgsProcessingAlgorithm):
             context,
             out_fields,
             source.wkbType(),
-            source.sourceCrs()
+            source.sourceCrs(),
         )
-        
+
         if sink is None:
             raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
 
--- sketch_map_tool/upload_processing/qgis_scripts/split_count_merge.py
+++ sketch_map_tool/upload_processing/qgis_scripts/split_count_merge.py
@@ -3,22 +3,45 @@ For each colour that has been used for markings, count overlapping features,
 i.e. overlapping markings in that colour from different sketch maps.
 """
 
-from qgis.core import QgsProcessing
-from qgis.core import QgsProcessingAlgorithm
-from qgis.core import QgsProcessingMultiStepFeedback
-from qgis.core import QgsProcessingParameterVectorLayer
-from qgis.core import QgsProcessingParameterString
-from qgis.core import QgsProcessingParameterFeatureSink
 import processing
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingMultiStepFeedback,
+    QgsProcessingParameterFeatureSink,
+    QgsProcessingParameterString,
+    QgsProcessingParameterVectorLayer,
+)
 
 
 # TODO: Refactor
 class Split_count_merge(QgsProcessingAlgorithm):
-
     def initAlgorithm(self, config=False):
-        self.addParameter(QgsProcessingParameterVectorLayer('inlayer', 'Input Layer', types=[QgsProcessing.TypeVectorPolygon], defaultValue=None))
-        self.addParameter(QgsProcessingParameterString('uniqueidfield', 'Colour field name', multiLine=False, defaultValue='color'))
-        self.addParameter(QgsProcessingParameterFeatureSink('OutputLayer', 'Output Layer', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                "inlayer",
+                "Input Layer",
+                types=[QgsProcessing.TypeVectorPolygon],
+                defaultValue=None,
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterString(
+                "uniqueidfield",
+                "Colour field name",
+                multiLine=False,
+                defaultValue="color",
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                "OutputLayer",
+                "Output Layer",
+                type=QgsProcessing.TypeVectorAnyGeometry,
+                createByDefault=True,
+                defaultValue=None,
+            )
+        )
 
     def processAlgorithm(self, parameters, context, model_feedback):
         feedback = QgsProcessingMultiStepFeedback(1, model_feedback)
@@ -26,81 +49,97 @@ class Split_count_merge(QgsProcessingAlgorithm):
         outputs = {}
 
         alg_params = {
-            'FIELDS': parameters['uniqueidfield'],
-            'INPUT': parameters['inlayer'],
-            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT,
-            'OUTPUT_HTML_FILE': QgsProcessing.TEMPORARY_OUTPUT
+            "FIELDS": parameters["uniqueidfield"],
+            "INPUT": parameters["inlayer"],
+            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            "OUTPUT_HTML_FILE": QgsProcessing.TEMPORARY_OUTPUT,
         }
         unique_id_values = processing.run(
-            'qgis:listuniquevalues', alg_params, context=context, feedback=feedback,
-            is_child_algorithm=True)['UNIQUE_VALUES'].split(';')
+            "qgis:listuniquevalues",
+            alg_params,
+            context=context,
+            feedback=feedback,
+            is_child_algorithm=True,
+        )["UNIQUE_VALUES"].split(";")
         out_layers = []
         for value in unique_id_values:
             alg_params = {
-                'FIELD': parameters['uniqueidfield'],
-                'INPUT': parameters['inlayer'],
-                'OPERATOR': 0,
-                'VALUE': value,
-                'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+                "FIELD": parameters["uniqueidfield"],
+                "INPUT": parameters["inlayer"],
+                "OPERATOR": 0,
+                "VALUE": value,
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
             }
 
             # TODO: What does this script do?
-            extracted_by_attribute = processing.run('native:extractbyattribute',
-                                     alg_params, context=context,
-                                     feedback=feedback,
-                                     is_child_algorithm=True)['OUTPUT']
+            extracted_by_attribute = processing.run(
+                "native:extractbyattribute",
+                alg_params,
+                context=context,
+                feedback=feedback,
+                is_child_algorithm=True,
+            )["OUTPUT"]
 
             alg_params = {
-                'INPUT': extracted_by_attribute,
-                'OVERLAY': None,
-                'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+                "INPUT": extracted_by_attribute,
+                "OVERLAY": None,
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
             }
 
             union = processing.run(
-                'native:union', alg_params, context=context, feedback=feedback,
-                is_child_algorithm=True)['OUTPUT']
+                "native:union",
+                alg_params,
+                context=context,
+                feedback=feedback,
+                is_child_algorithm=True,
+            )["OUTPUT"]
             feedback.setCurrentStep(2)
             if feedback.isCanceled():
                 return {}
 
-            alg_params = {
-                'INPUT': union,
-                'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
-            }
+            alg_params = {"INPUT": union, "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT}
             # Run custom count_duplicates script (also needs to be added to the QGIS processing toolbox before
             # running this one)
             # TODO: Add code from second script here
             overlap_counts = processing.run(
-                'script:count_duplicates', alg_params, context=context, feedback=feedback,
-                is_child_algorithm=True)['OUTPUT']
+                "script:count_duplicates",
+                alg_params,
+                context=context,
+                feedback=feedback,
+                is_child_algorithm=True,
+            )["OUTPUT"]
             out_layers.append(overlap_counts)
-        
+
         # Merge vector layers
         alg_params = {
-            'CRS': parameters['inlayer'],
-            'LAYERS': out_layers,
-            'OUTPUT': parameters['OutputLayer']
+            "CRS": parameters["inlayer"],
+            "LAYERS": out_layers,
+            "OUTPUT": parameters["OutputLayer"],
         }
 
-        results['OutputLayer'] = processing.run(
-            'native:mergevectorlayers', alg_params, context=context, feedback=feedback,
-            is_child_algorithm=True)['OUTPUT']
+        results["OutputLayer"] = processing.run(
+            "native:mergevectorlayers",
+            alg_params,
+            context=context,
+            feedback=feedback,
+            is_child_algorithm=True,
+        )["OUTPUT"]
         return results
-        
+
     def name(self):
-        return 'split_count_merge'
+        return "split_count_merge"
 
     def displayName(self):
-        return 'split_count_merge'
+        return "split_count_merge"
 
     def group(self):
-        return ''
+        return ""
 
     def groupId(self):
-        return ''
-    
+        return ""
+
     def shortHelpString(self):
-        return 'Split input layer by unique colours. Count duplicates for every unique value found. Merge resulting layers into final Output Layer, containing counts of overlapping features for each unique attribute value in specified id_field.'
+        return "Split input layer by unique colours. Count duplicates for every unique value found. Merge resulting layers into final Output Layer, containing counts of overlapping features for each unique attribute value in specified id_field."
 
     def createInstance(self):
         return Split_count_merge()
