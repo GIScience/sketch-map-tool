d833dd57f3e62cfd79e2adce2b10aacc7b859622
jstier <stier.jochen@web.de>
2023-08-21 16:10:29 +0200

    fix: adjust to new opt schema

--- tests/unit/test_oqt_client.py
+++ tests/unit/test_oqt_client.py
@@ -1,24 +1,32 @@
-from sketch_map_tool.oqt_analyses.oqt_client import bbox_to_polygon, get_report
+from sketch_map_tool.oqt_analyses.oqt_client import (
+    bbox_to_feature_collection,
+    get_report,
+)
 from tests import vcr_app as vcr
 
 
-def test_bbox_to_polygon(bbox_wgs84):
+def test_bbox_to_feature_collection(bbox_wgs84):
     expected = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                [
-                    [8.625, 49.3711],
-                    [8.7334, 49.3711],
-                    [8.7334, 49.4397],
-                    [8.625, 49.4397],
-                    [8.625, 49.3711],
-                ]
-            ],
-        },
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [8.625, 49.3711],
+                            [8.7334, 49.3711],
+                            [8.7334, 49.4397],
+                            [8.625, 49.4397],
+                            [8.625, 49.3711],
+                        ]
+                    ],
+                },
+            }
+        ],
     }
-    assert expected == bbox_to_polygon(bbox_wgs84)
+    assert expected == bbox_to_feature_collection(bbox_wgs84)
 
 
 # todo improve test
