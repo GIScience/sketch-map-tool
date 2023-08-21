4bb0d787586a2d48ca25b75af9e6ef352e67769e
jstier <stier.jochen@web.de>
2023-08-21 16:10:29 +0200

    fix: adjust to new opt schema

--- sketch_map_tool/oqt_analyses/oqt_client.py
+++ sketch_map_tool/oqt_analyses/oqt_client.py
@@ -7,33 +7,33 @@ OQT_API_URL = "https://oqt.ohsome.org/api"
 OQT_REPORT_NAME = "sketchmap-fitness"
 
 
-def bbox_to_polygon(bbox: Bbox) -> dict:
+def bbox_to_feature_collection(bbox: Bbox) -> dict:
     polygon = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                [
-                    [bbox.lon_min, bbox.lat_min],
-                    [bbox.lon_max, bbox.lat_min],
-                    [bbox.lon_max, bbox.lat_max],
-                    [bbox.lon_min, bbox.lat_max],
-                    [bbox.lon_min, bbox.lat_min],
-                ]
-            ],
-        },
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [bbox.lon_min, bbox.lat_min],
+                            [bbox.lon_max, bbox.lat_min],
+                            [bbox.lon_max, bbox.lat_max],
+                            [bbox.lon_min, bbox.lat_max],
+                            [bbox.lon_min, bbox.lat_min],
+                        ]
+                    ],
+                },
+            }
+        ],
     }
     return polygon
 
 
 def get_report(bbox: Bbox, include_svg: bool = True, include_html: bool = False):
     url = OQT_API_URL + "/" + "reports" + "/" + OQT_REPORT_NAME
-    parameters = {
-        "bpolys": bbox_to_polygon(bbox),
-        "includeSvg": include_svg,
-        "includeHtml": include_html,
-        "flatten": False,
-    }
+    parameters = {"bpolys": bbox_to_feature_collection(bbox)}
     req = requests.post(url, json=parameters)
     if req.status_code == 422:
         if req.json()["type"] == "SizeRestrictionError":
@@ -48,5 +48,4 @@ def get_report(bbox: Bbox, include_svg: bool = True, include_html: bool = False)
         raise OQTReportError(
             "There seems to be a problem with OQT. Please try again later."
         )
-    report_properties = req.json()["properties"]
-    return report_properties
+    return req.json()["features"][0]["properties"]
