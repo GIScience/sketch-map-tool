007df36c33ad93c6c761fd607625fc272c022c04
jstier <stier.jochen@web.de>
2023-10-11 14:59:43 +0200

    fix: reproject geojson to valid EPSG:4326 before returning to user

--- sketch_map_tool/upload_processing/polygonize.py
+++ sketch_map_tool/upload_processing/polygonize.py
@@ -5,6 +5,44 @@ from tempfile import NamedTemporaryFile, TemporaryDirectory
 from osgeo import gdal, ogr, osr
 
 
+def reproject_geojson(
+    layer_name,
+    srs_srs,
+    outfile_name: str,
+    reprojected_name: str,
+) -> None:
+    """Reproject GeoJSON from WebMercator to EPSG:4326"""
+    # create transformation
+    out_spatial_ref = osr.SpatialReference()
+    out_spatial_ref.ImportFromEPSG(4326)
+    coord_trans = osr.CreateCoordinateTransformation(srs_srs, out_spatial_ref)
+
+    # load/create in and reprojected layers
+    driver = ogr.GetDriverByName("GeoJSON")
+    in_data_set = driver.Open(outfile_name)
+    in_layer = in_data_set.GetLayer()
+
+    reproj_ds = driver.CreateDataSource(reprojected_name)
+    reproj_layer = reproj_ds.CreateLayer(layer_name, srs=out_spatial_ref)
+    reproj_layer_definition = reproj_layer.GetLayerDefn()
+
+    # loop though features in input, reproject and write to output
+    in_feature = in_layer.GetNextFeature()
+    while in_feature:
+        geom = in_feature.GetGeometryRef()
+
+        geom.Transform(coord_trans)
+
+        out_feature = ogr.Feature(reproj_layer_definition)
+        out_feature.SetGeometry(geom)
+        reproj_layer.CreateFeature(out_feature)
+
+        out_feature = None
+        in_feature = in_layer.GetNextFeature()
+    reproj_ds = None
+    in_data_set = None
+
+
 def polygonize(geotiff: BytesIO, layer_name: str) -> BytesIO:
     """Produces a polygon feature layer (GeoJSON) from a raster (GeoTIFF)."""
     gdal.UseExceptions()
@@ -14,11 +52,13 @@ def polygonize(geotiff: BytesIO, layer_name: str) -> BytesIO:
     infile = NamedTemporaryFile(suffix=".geotiff")
     with open(infile.name, "wb") as f:
         f.write(geotiff.getbuffer())
+
     src_ds = gdal.Open(infile.name)
-    srs = osr.SpatialReference(wkt=src_ds.GetProjection())
+    srs = src_ds.GetSpatialRef()
 
     with TemporaryDirectory() as tmpdirname:
         outfile_name = Path(tmpdirname) / "out.geojson"
+        reprojected_name = Path(tmpdirname) / "reprojected.geojson"
 
         # open geojson
         driver = ogr.GetDriverByName("GeoJSON")
@@ -33,5 +73,8 @@ def polygonize(geotiff: BytesIO, layer_name: str) -> BytesIO:
 
         src_ds = None  # close dataset
         dst_ds = None  # close dataset
-        with open(outfile_name, "rb") as f:
+
+        reproject_geojson(layer_name, srs, str(outfile_name), str(reprojected_name))
+
+        with open(reprojected_name, "rb") as f:
             return BytesIO(f.read())
