67fd6c0b3b70cab2f3531c5189a7c8078a47aecb
jstier <stier.jochen@web.de>
2023-10-11 15:50:52 +0200

    test: add geojson test for validity

--- tests/unit/upload_processing/test_polygonize.py
+++ tests/unit/upload_processing/test_polygonize.py
@@ -1,8 +1,12 @@
 from io import BytesIO
 
+import geojson
+
 from sketch_map_tool.upload_processing import polygonize
 
 
 def test_polygonize(sketch_map_frame_markings_detected_buffer):
     geojson_buffer = polygonize(sketch_map_frame_markings_detected_buffer, "red")
+    json = geojson.loads(geojson_buffer.read())
+    assert json.is_valid
     assert isinstance(geojson_buffer, BytesIO)
