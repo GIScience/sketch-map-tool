6359b6f9fd4b3b7e749ce1c574128fabefb146ff
jstier <stier.jochen@web.de>
2023-10-11 11:34:12 +0200

    tests(flask): add routes to flast test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,14 @@ from sketch_map_tool import make_flask
 from sketch_map_tool.database import client_celery as db_client_celery
 from sketch_map_tool.database import client_flask as db_client_flask
 from sketch_map_tool.models import Bbox, PaperFormat, Size
-from sketch_map_tool.routes import digitize_results_post
+from sketch_map_tool.routes import (
+    about,
+    digitize,
+    digitize_results_get,
+    digitize_results_post,
+    help,
+    index,
+)
 from tests import FIXTURE_DIR
 
 
@@ -64,6 +71,13 @@ def flask_client(flask_app):
     flask_app.add_url_rule(
         "/digitize/results", view_func=digitize_results_post, methods=["POST"]
     )
+    flask_app.add_url_rule(
+        "/digitize/results", view_func=digitize_results_get, methods=["GET"]
+    )
+    flask_app.add_url_rule("/digitize", view_func=digitize, methods=["GET"])
+    flask_app.add_url_rule("/", view_func=index, methods=["GET"])
+    flask_app.add_url_rule("/about", view_func=about, methods=["GET"])
+    flask_app.add_url_rule("/help", view_func=help, methods=["GET"])
     return flask_app.test_client()
 
 
