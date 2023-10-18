3436b912a9b991f0c7238a6edf9614f5fdcdf537
jstier <stier.jochen@web.de>
2023-10-18 11:26:11 +0200

    merge

--- client-src/shared/poll/pollUntilValid.js
+++ client-src/shared/poll/pollUntilValid.js
@@ -27,24 +27,8 @@ class PollUntilValid {
         onError = () => {},
     ) {
         do {
-            let response;
-            try {
-                /* eslint-disable no-await-in-loop */
-                response = await fetch(url);
-            } catch (error) {
-                if (error instanceof TypeError) {
-                    // Chrome and Firefox use different Error messages, so it's hard to be more
-                    // specific than checking for TypeError
-                    // see: https://developer.mozilla.org/en-US/docs/Web/API/fetch#exceptions
-                    console.log("NetworkError, continue retrying", error);
-                    await PollUntilValid.wait(5000);
-                } else {
-                    throw error;
-                }
-            }
-
-            // eslint-disable-next-line no-continue
-            if (!response) continue;
+            /* eslint-disable no-await-in-loop */
+            const response = await fetch(url);
 
             // if response code is not between 200-299
             if (!response.ok) {
@@ -60,7 +44,7 @@ class PollUntilValid {
                 return response;
             }
 
-            /* eslint-enable */
+            // eslint-disable-next-line no-constant-condition
         } while (true);
     }
 
--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -8,21 +8,21 @@ import requests
 
 from tests import vcr_app as vcr
 
+a4 = {
+    "format": "A4",
+    "orientation": "landscape",
+    "bbox": ("[964445.3646475708,6343463.48326091,967408.255014792,6345943.466874749]"),
+    "bboxWGS84": (
+        "[8.66376011761138,49.40266507327297,8.690376214631833,49.41716014123875]"
+    ),
+    "size": '{"width": 1716,"height": 1436}',
+    "scale": "9051.161965312804",
+}
 
-@pytest.fixture(scope="session")
-def params():
-    return {
-        "format": "A4",
-        "orientation": "landscape",
-        "bbox": (
-            "[964445.3646475708,6343463.48326091,967408.255014792,6345943.466874749]"
-        ),
-        "bboxWGS84": (
-            "[8.66376011761138,49.40266507327297,8.690376214631833,49.41716014123875]"
-        ),
-        "size": '{"width": 1716,"height": 1436}',
-        "scale": "9051.161965312804",
-    }
+# TODO: Add other params
+@pytest.fixture(scope="session", params=[a4])
+def params(request):
+    return request.param
 
 
 @pytest.fixture(scope="session")
@@ -38,9 +38,8 @@ def uuid(params):
 @pytest.fixture(scope="session")
 def sketch_map_pdf(uuid, tmp_path_factory) -> Path:
     fn = tmp_path_factory.mktemp(uuid, numbered=False) / "sketch-map.pdf"
-    # for 20 seconds check status
-    # TODO: decide on time to wait
-    for _ in range(20):
+    # for 30 seconds check status
+    for _ in range(30):
         response = requests.get(f"http://localhost:8081/api/status/{uuid}/sketch-map")
         if response.status_code == 200:
             response = requests.get(
@@ -50,10 +49,10 @@ def sketch_map_pdf(uuid, tmp_path_factory) -> Path:
                 file.write(response.content)
             return fn
         sleep(1)
-    raise TimeoutError("Sketch map PDF not created")
+    raise TimeoutError("Sketch map PDF could not be created")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def sketch_map_png(sketch_map_pdf, tmp_path_factory, uuid):
     with open(sketch_map_pdf, "rb") as file:
         sketch_map_pdf = file.read()
@@ -63,6 +62,22 @@ def sketch_map_png(sketch_map_pdf, tmp_path_factory, uuid):
     path = tmp_path_factory.getbasetemp() / uuid / "sketch-map.png"
     png.save(path, output="png")
     return path
+
+
+def test_sketch_map_pdf(sketch_map_pdf):
+    # test if fixture is created successfully
+    with open(sketch_map_pdf, "rb") as file:
+        sketch_map_pdf = file.read()
+    assert sketch_map_pdf is not None
+    assert len(sketch_map_pdf) > 0
+
+
+def test_sketch_map_png(sketch_map_png):
+    # test if fixture is created successfully
+    with open(sketch_map_png, "rb") as file:
+        sketch_map_png = file.read()
+    assert sketch_map_png is not None
+    assert len(sketch_map_png) > 0
 
 
 @vcr.use_cassette
@@ -80,21 +95,29 @@ def test_create_results_post(params):
 
 
 def test_api_status_uuid_sketch_map(uuid):
-    # for 20 seconds check status
-    # TODO: decide on time to wait
-    for _ in range(20):
+    # for 30 seconds check status
+    for _ in range(30):
         response = requests.get(f"http://localhost:8081/api/status/{uuid}/sketch-map")
         assert response.status_code in (200, 202)
+        result = response.json()
+
+        status = result.pop("status")
+        assert status in ("SUCCESS", "PENDING", "RETRY", "STARTED")
+
+        href = result.pop("href", None)
+        assert result == {"id": uuid, "type": "sketch-map"}
+
         if response.status_code == 200:
+            assert href == "/api/download/" + uuid + "/sketch-map"
+            assert status == "SUCCESS"
             break
         sleep(1)
     assert response.status_code == 200
 
 
 def test_api_download_uuid_sketch_map(uuid):
-    # for 20 seconds check status
-    # TODO: decide on time to wait
-    for _ in range(20):
+    # for 30 seconds check status
+    for _ in range(30):
         response = requests.get(f"http://localhost:8081/api/status/{uuid}/sketch-map")
         if response.status_code == 200:
             response = requests.get(
@@ -103,6 +126,7 @@ def test_api_download_uuid_sketch_map(uuid):
             break
         sleep(1)
     assert response.status_code == 200
+    assert len(response.content) > 0
 
 
 def test_digitize_results_post(sketch_map_png):
@@ -118,19 +142,3 @@ def test_digitize_results_post(sketch_map_png):
     url_rest = "/".join(url_parts[:-1])
     assert UUID(uuid).version == 4
     assert url_rest == "http://localhost:8081/digitize/results"
-
-
-def test_sketch_map_pdf(sketch_map_pdf):
-    # test fixture is created successfully
-    with open(sketch_map_pdf, "rb") as file:
-        sketch_map_pdf = file.read()
-    assert sketch_map_pdf is not None
-    assert len(sketch_map_pdf) > 0
-
-
-def test_sketch_map_png(sketch_map_png):
-    # test fixture is created successfully
-    with open(sketch_map_png, "rb") as file:
-        sketch_map_png = file.read()
-    assert sketch_map_png is not None
-    assert len(sketch_map_png) > 0
