0fd136b4f2b512ca64987715c3a8e9e120f54209
Matthias Schaub <matthias.schaub@heigit.org>
2023-10-19 10:51:56 +0200

    test: refactor tests to use testcontainers

--- tests/integration/test_celery_tasks.py
+++ tests/integration/test_celery_tasks.py
@@ -2,20 +2,27 @@ from io import BytesIO
 from uuid import uuid4
 
 from sketch_map_tool import tasks
+from tests import vcr_app
 
 
-def test_generate_sketch_map(
-    celery_worker, db_conn_celery, uuid, bbox, format_, size, scale
-):
-
+@vcr_app.use_cassette
+def test_generate_sketch_map(bbox, format_, size, scale):
     task = tasks.generate_sketch_map.apply_async(
-        args=(str(uuid4()), bbox, format_, "landscape", size, scale)
+        args=(
+            str(uuid4()),
+            bbox,
+            format_,
+            "landscape",
+            size,
+            scale,
+        )
     )
     result = task.wait()
     assert isinstance(result, BytesIO)
 
 
-def test_generate_quality_report(celery_app, celery_worker, db_conn_celery, bbox_wgs84):
+@vcr_app.use_cassette
+def test_generate_quality_report(bbox_wgs84):
     task = tasks.generate_quality_report.apply_async(args=[bbox_wgs84])
     result = task.wait()
     assert isinstance(result, BytesIO)
