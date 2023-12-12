59db819c629d5d2ef749df6a36fa9a79e68dc35d
Matthias Schaub <matthias.schaub@heigit.org>
2023-12-12 09:20:19 +0100

    refactor(ml-models): avoid connecting to neptune
    only connect to neptune.ai if no model is present

--- sketch_map_tool/upload_processing/ml_models.py
+++ sketch_map_tool/upload_processing/ml_models.py
@@ -5,41 +5,31 @@ import neptune
 
 from sketch_map_tool.config import get_config_value
 
-PROJECT = get_config_value("neptune_project")
-API_TOKEN = get_config_value("neptune_api_token")
-
 
 def init_model(id: str) -> Path:
-    """Initilaze model. Download model to data dir if not present."""
-    # TODO:
-    # _check_id(id)
-
-    data_dir = Path(get_config_value("data-dir"))
-    # TODO: first check if model is on disk
-    # if so return model without connecting to neptune.ai
-    model = neptune.init_model_version(
-        with_id=id,
-        project=PROJECT,
-        api_token=API_TOKEN,
-        mode="read-only",
-    )
-
-    raw = data_dir / id
+    """Initialize model. Download model to data dir if not present."""
+    # TODO: _check_id(id)
+    # TODO: check if model is valid/working
+    raw = Path(get_config_value("data-dir")) / id
     path = raw.with_suffix(_get_file_suffix(id))
     if not path.is_file():
         logging.info(f"Downloading model {id} from neptune.ai to {path}.")
+        model = neptune.init_model_version(
+            with_id=id,
+            project=get_config_value("neptune_project"),
+            api_token=get_config_value("neptune_api_token"),
+            mode="read-only",
+        )
         model["model"].download(str(path))
-
-    # TODO: check if model is valid/working
-    logging.info("Available model from neptune.ai on disk: " + id)
+        return path
     return path
 
 
 def _check_id(id: str):
     # TODO:
     project = neptune.init_project(
-        project=PROJECT,
-        api_token=API_TOKEN,
+        project=get_config_value("neptune_project"),
+        api_token=get_config_value("neptune_api_token"),
         mode="read-only",
     )
 
