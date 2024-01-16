0195073ff6c4bc946470dc23d558dd831309f650
Matthias Schaub <matthias.schaub@heigit.org>
2024-01-16 12:07:50 +0100

    fix: do not write CRS property to GeoJSON

--- sketch_map_tool/upload_processing/merge.py
+++ sketch_map_tool/upload_processing/merge.py
@@ -9,11 +9,9 @@ def merge(fcs: list[FeatureCollection]) -> FeatureCollection:
     features = []
     for fc in fcs:
         color = fc.get("name", "foo")
-        crs = fc.get("crs", "crs")
         for f in fc.features:
             properties = f.properties
             properties["color"] == color
             features.append(Feature(geometry=f.geometry, properties=properties))
     feature_collection = FeatureCollection(features=features)
-    feature_collection["crs"] = crs
     return feature_collection
