412e0d0c2d6b0242a007091828972d6ad6d0f2c3
Michael Auer <michael.auer@heigit.org>
2024-01-23 18:31:06 +0100

    feat(frontend): read baselayer from URL and add ESRI World_Imagery as XYZ Layer to the map

--- client-src/create/index.js
+++ client-src/create/index.js
@@ -8,9 +8,10 @@ import { addGeocoderControl, addPrintLayoutControl, createMap } from "./map.js";
 import { bindFormToPrintLayoutControl } from "./form.js";
 import { MessageController } from "./messageController";
 
-// Retrieve potentially given map center from URL (e.g. from a bookmarked selection)
+// Retrieve potentially given map center and baselayer from URL (e.g. from a bookmarked selection)
 const searchParams = new URLSearchParams(window.location.search);
 const centerArg = searchParams.get("center");
+const baselayerArg = searchParams.get("baselayer");
 
 let center = [966253.1800856147, 6344703.99262965];
 if (centerArg != null) {
@@ -18,7 +19,19 @@ if (centerArg != null) {
     center = [parseFloat(centerCoords[0]), parseFloat(centerCoords[1])];
 }
 
-const map = createMap("map", center, 15);
+// type BaselayerType = "OSM" | "ESRI:WorldImagery"
+let baselayer;
+if (baselayerArg != null) {
+    switch (baselayerArg) {
+        case "ESRI:World_Imagery":
+            baselayer = baselayerArg;
+            break;
+        default:
+            baselayer = "OSM";
+    }
+}
+
+const map = createMap("map", center, 15, baselayer);
 
 const printLayoutControl = addPrintLayoutControl(map);
 const messageController = new MessageController();
