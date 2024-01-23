a2443a485ddf65724c665a0ba0004008a92d1001
Michael Auer <michael.auer@heigit.org>
2024-01-23 18:31:06 +0100

    feat(frontend): read baselayer from URL and add ESRI World_Imagery as XYZ Layer to the map

--- client-src/create/map.js
+++ client-src/create/map.js
@@ -1,8 +1,8 @@
 import { Feature, Map, View } from "ol";
 import { Tile } from "ol/layer";
-import { OSM } from "ol/source";
+import { OSM, XYZ } from "ol/source";
 import Geocoder from "ol-geocoder";
-import { PrintLayout, PAPER_FORMAT, ORIENTATION } from "@giscience/ol-print-layout-control";
+import { ORIENTATION, PAPER_FORMAT, PrintLayout } from "@giscience/ol-print-layout-control";
 import { fromLonLat } from "ol/proj";
 import { LineString } from "ol/geom";
 import VectorSource from "ol/source/Vector";
@@ -54,9 +54,19 @@ function createAntiMeridianLayer() {
  * @param {number[]} [lonLat=[8.68, 49.41]] - an Array with two numbers representing
  *     latitude and longitude
  * @param {number} [zoom=15] - the zoomlevel in which the map will be initialized
+ * @param {string} [baselayer="OSM"] - the baselayer to be displayed 'OSM' | 'ESRI:World_Imagery'
  * @returns {Map}
  */
-function createMap(target = "map", lonLat = [966253.1800856147, 6344703.99262965], zoom = 15) {
+function createMap(target = "map", lonLat = [966253.1800856147, 6344703.99262965], zoom = 15, baselayer = "OSM") {
+    const esriApiKey = "AAPKe1520d4c006c4b19941f009329c58e22bGmaXWiqR_M_Y0gPJVEcYNjplvkD1n4bIukV28vgPe1ZVTdNl6OTKI5uNsX9BzC2";
+    const esriWorldImageryLayer = new Tile({
+        visible: baselayer === "ESRI:World_Imagery",
+        source: new XYZ({
+            url: `https://ibasemaps-api.arcgis.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}?token=${esriApiKey}`,
+            // TODO attribution
+        }),
+    });
+
     const map = new Map({
         target,
         view: new View({
@@ -67,8 +77,10 @@ function createMap(target = "map", lonLat = [966253.1800856147, 6344703.99262965
         }),
         layers: [
             new Tile({
+                visible: baselayer !== "ESRI:World_Imagery",
                 source: new OSM(),
             }),
+            esriWorldImageryLayer,
             createAntiMeridianLayer(),
         ],
     });
