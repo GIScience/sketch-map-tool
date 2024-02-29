55a4f85f145229fd5ba988479cfe862295761928
Matthias Schaub <matthias.schaub@heigit.org>
2024-02-29 14:49:39 +0100

    docs: add comments to base.js

--- client-src/base/base.js
+++ client-src/base/base.js
@@ -6,10 +6,13 @@ function setLanguage(lang) {
     const path = window.location.pathname.slice(1); // remove first slash
     const parts = path.split("/");
     if (languages.includes(parts[0])) {
+        // overwrite
         parts[0] = lang;
     } else {
+        // prepend
         parts.unshift(lang);
     }
+    // remove trailing slash
     window.location.pathname = parts.join("/").replace(/\/$/, "");
 }
 
