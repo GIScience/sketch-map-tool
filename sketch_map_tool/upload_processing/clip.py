f89beeedbcd37348d5cba021336d7847d5219223
Yulia Grinblat <solo2307@gmail.com>
2024-03-25 10:30:03 +0100

    Optimizing clipping function (#387)
    * optimizing matching search + filtering bad transformation
    
    * docs: update development documentation
    
    * fix(clip): add workaround for cases where no match is found
    
    * fix: fix faulty shape of zeroes array solving issue #396
    
    * ui: update estimated run time on pages
    
    ---------
    
    Co-authored-by: ygrinblat <ygrinblat@heigit.org>
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>
    Co-authored-by: Levi Szamek <levi@szamek.de>
    Co-authored-by: itisacloud <clemens.langer@stud.uni-heidelberg.de>

--- sketch_map_tool/upload_processing/clip.py
+++ sketch_map_tool/upload_processing/clip.py
@@ -14,25 +14,73 @@ def clip(photo: NDArray, template: NDArray) -> NDArray:
     photo based on a given template with the map. The detected area of the
     image is warped to have the same alignment as the template.
 
+    Utilizes the FLANN (Fast Library for Approximate Nearest Neighbors)
+    algorithm for efficient
+    descriptor matching.
+
     :param photo: Photograph of a sketch map
     :param template: Matching template of the sketch map
     :return: The resulting image (the cutout)
     """
     brisk = cv2.BRISK_create()
+
+    # Convert images to grayscale
     photo_gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
     template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
+    # Detect keypoints and compute descriptors
     kpts1, desc1 = brisk.detectAndCompute(photo_gray, None)
     kpts2, desc2 = brisk.detectAndCompute(template_gray, None)
 
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = list(matcher.match(desc1, desc2))
-    # Sort the matches by distance
-    matches = sorted(matches, key=lambda x: x.distance)
-    good_matches = matches[0 : int(len(matches) * 0.10)]
+    # FLANN parameters
+    flann_params = {
+        "algorithm": 6,
+        "table_number": 6,
+        "key_size": 12,
+        "multi_probe_level": 1,
+    }
+
+    # FLANN matcher
+    matcher = cv2.FlannBasedMatcher(flann_params, {})
+    matches = matcher.knnMatch(queryDescriptors=desc1, trainDescriptors=desc2, k=2)
+
+    # Filter good matches
+    good_matches = []
+    for match in matches:
+        # TODO: evaluate workaround
+        if len(match) != 2:
+            continue
+        if match[0].distance < 0.75 * match[1].distance:
+            good_matches.append(match[0])
+
+    # Extract corresponding points
     src_pts = np.float32([kpts1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
     dst_pts = np.float32([kpts2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+    # Find homography matrix
     homography_matrix, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
+    # Get dimensions of template
     height, width, _ = template.shape
-    selected_map = cv2.warpPerspective(photo, homography_matrix, (width, height))
+
+    # Check if homography matrix passes filter
+    succeed = filter_matrix(homography_matrix)
+
+    # Warp perspective if homography matrix passes filter
+    if succeed:
+        selected_map = cv2.warpPerspective(photo, homography_matrix, (width, height))
+    else:
+        np.zeros(template.shape, dtype=np.uint8)
     return selected_map
+
+
+def filter_matrix(tran_matrix: NDArray) -> bool:
+    """Filters a failed transformation matrix based on specified conditions."""
+    h13 = tran_matrix[0, 2]
+    h23 = tran_matrix[1, 2]
+
+    if h13 > 1500 and h23 > 0:
+        print(False)
+        return False
+    else:
+        print(True)
+        return True
