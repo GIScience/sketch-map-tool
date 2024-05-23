e0ea5a4d21d6c18d7d2bb05e32322b51ac3bc3d0
Matthias Schaub <matthias.schaub@heigit.org>
2024-05-23 11:10:41 +0200

    test(approval): use epsg:8857 for area comparison

--- tests/comparator.py
+++ tests/comparator.py
@@ -9,9 +9,9 @@ class GeoJSONComparator(FileComparator):
         if not Path(approved_path).exists() or Path(approved_path).stat().st_size == 0:
             return False
 
-        # TODO: find optimal UTM for every feature or different projection
-        df_received = geopandas.read_file(received_path).to_crs("EPSG:32632")
-        df_approved = geopandas.read_file(approved_path).to_crs("EPSG:32632")
+        # EPSG:8857 - small scale equal-area mapping
+        df_received = geopandas.read_file(received_path).to_crs("EPSG:8857")
+        df_approved = geopandas.read_file(approved_path).to_crs("EPSG:8857")
 
         if len(df_approved) != len(df_received):
             return False
@@ -20,7 +20,6 @@ class GeoJSONComparator(FileComparator):
         area_diff = df_approved.area - area_clipped
         area_ratio = area_diff / df_approved.area
         for r in area_ratio:
-            # TODO: what is the right threshold
             if r > 0.01:
                 return False
         return True
