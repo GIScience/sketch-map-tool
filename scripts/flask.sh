756ee1611f182c72535fc057ed846df3aae78101
Matthias Schaub <matthias.schaub@heigit.org>
2024-08-27 10:07:00 +0200

    chore: update flask run script for dev

--- scripts/flask.sh
+++ scripts/flask.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Run Flask in debug mode
-pybabel compile -d sketch_map_tool/translations
-flask --app sketch_map_tool/routes.py --debug run --port 8081
+npm run build
+poetry run pybabel compile -d sketch_map_tool/translations
+poetry run flask --app sketch_map_tool/routes.py --debug run --port 8081
