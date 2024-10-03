4e2fdf68488d339a4b538798f78e0b2314663424
Matthias Schaub <matthias.schaub@heigit.org>
2024-10-03 02:15:04 +0200

    test: fix failing tests due to missing route
    declaration in conftest. The missing route was added in
    d4557fb3d8ffb52c17899e7e17fc624458632f8b

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -149,6 +149,11 @@ def flask_app():
     app.add_url_rule("/", view_func=routes.index, methods=["GET"])
     app.add_url_rule("/about", view_func=routes.about, methods=["GET"])
     app.add_url_rule("/help", view_func=routes.help, methods=["GET"])
+    app.add_url_rule(
+        "/case-studies/cultural-landmarks",
+        view_func=routes.case_study_cultural_landmarks,
+        methods=["GET"],
+    )
 
     Babel(app, locale_selector=get_locale)  # for translations
 
