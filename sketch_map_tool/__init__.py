aa8ee54d879eb5083590ca38c06d6192e169b54c
Rafael Troilo <rafael.troilo@heigit.org>
2025-03-03 17:23:11 +0100

    Release 2025.03.03.2

--- sketch_map_tool/__init__.py
+++ sketch_map_tool/__init__.py
@@ -13,7 +13,7 @@ from sketch_map_tool.config import get_config_value
 from sketch_map_tool.database import client_flask as db_client
 from sketch_map_tool.definitions import LANGUAGES
 
-__version__ = "2025.03.03.1"
+__version__ = "2025.03.03.2"
 
 # Setup logging
 LEVEL = getattr(logging, get_config_value("log-level").upper())
