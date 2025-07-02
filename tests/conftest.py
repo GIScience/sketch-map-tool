c3eeb1270636781bf19d4afe8acea994dfc2a4ca
Matthias Schaub <matthias.schaub@heigit.org>
2025-07-02 14:24:12 +0200

    test: remove legacy fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,11 @@
 import pytest
 from vcr.stubs import VCRHTTPResponse
 
-from sketch_map_tool.helpers import get_project_root
-
 
 @pytest.fixture(scope="session")
 def monkeypatch_session():
     with pytest.MonkeyPatch.context() as mp:
         yield mp
-
-
-@pytest.fixture(scope="session", autouse=True)
-def configuration(monkeypatch_session):
-    path = str(get_project_root() / "config" / "test.config.toml")
-    yield monkeypatch_session.setenv("SMT_CONFIG", path)
 
 
 # HACK: temporary workaround of https://github.com/kevin1024/vcrpy/issues/888
