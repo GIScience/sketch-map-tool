29b0de3a9ff8aaed6704703df8c02525a10030ea
Levi Szamek <levi.szamek@heigit.org>
2025-09-08 14:24:32 +0200

    feat(ui): add link to download detected markings as point only layer

--- client-src/digitize-results/index.js
+++ client-src/digitize-results/index.js
@@ -1,9 +1,11 @@
 import { getUUIDFromURL, poll, handleMainMessage } from "../shared";
 
 const vectorResultsUrl = `/api/status/${getUUIDFromURL()}/vector-results`;
+const centroidsResultsUrl = `/api/status/${getUUIDFromURL()}/centroid-results`;
 const rasterResultsUrl = `/api/status/${getUUIDFromURL()}/raster-results`;
 
 Promise.all([
     poll(rasterResultsUrl, "raster-data"),
     poll(vectorResultsUrl, "vector-data"),
+    poll(centroidsResultsUrl, "centroid-data"),
 ]).then(handleMainMessage);
