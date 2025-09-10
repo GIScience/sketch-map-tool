92b4de6d30e4b502344ba9d8eb7ec1f0ffcfa83b
Matthias Schaub <matthias.schaub@heigit.org>
2025-09-10 10:53:25 +0200

    fix: hide message container if no message is displayed on create page
    This fixes in issue that on mobile the empty message container took much
    vertical space between map and form. Which looked quite awkward.

--- client-src/create/messageController.js
+++ client-src/create/messageController.js
@@ -9,20 +9,21 @@ export class MessageController {
         if (this.activeMessageIds.has(elementId)) return;
 
         setDisabled("next-button", true);
-        document.querySelector(`#${elementId}`)
-            .classList
-            .remove("hidden");
-
+        document.querySelector("#message-container").hidden = false;
+        document.querySelector(`#${elementId}`).hidden = false;
         this.activeMessageIds.add(elementId);
     }
 
     removeWarning(elementId) {
         this.activeMessageIds.delete(elementId);
-        document.querySelector(`#${elementId}`)
-            .classList
-            .add("hidden");
+        document.querySelector(`#${elementId}`).hidden = true;
         if (this.activeMessageIds.size === 0) {
             setDisabled("next-button", false);
+        }
+        const messageContainer = document.querySelector("#message-container");
+        const shouldHide = [...messageContainer.children].every((child) => child.hidden);
+        if (shouldHide) {
+            messageContainer.hidden = true;
         }
     }
 }
