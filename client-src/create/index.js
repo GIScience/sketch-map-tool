9641f8c4c2aaf7d5934ec16cd861fc3736f586b4
Matthias Schaub <matthias.schaub@heigit.org>
2025-09-15 09:47:28 +0200

    feat(ui): sync form params with url query params (permalink)
    refactor(ui): use default of createMap function
    
    refactor: sanitize URL query params and set initial URL query params
    
    - remove alpine.js in favor of plain js handling of query params.
    - create URL helper module
    
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>
    Co-authored-by: Michael Auer <michael.aurer@heigit.org>

--- client-src/create/index.js
+++ client-src/create/index.js
@@ -4,6 +4,7 @@ import "ol-geocoder/dist/ol-geocoder.css";
 import "./geocoder.css";
 import "./create.css";
 
+import { PAPER_FORMAT, ORIENTATION } from "@giscience/ol-print-layout-control";
 import {
     addGeocoderControl,
     addLayerswitcherControl,
@@ -12,33 +13,69 @@ import {
 } from "./map.js";
 import { bindFormToLayerSwitcherControl, bindFormToPrintLayoutControl } from "./form.js";
 import { MessageController } from "./messageController";
+import { setAllQueryParam } from "../shared";
 
-// Retrieve potentially given map center and baselayer from URL (e.g. from a bookmarked selection)
-const searchParams = new URLSearchParams(window.location.search);
-const centerArg = searchParams.get("center");
-const baselayerArg = searchParams.get("layer");
+function getSanitizedUrlSearchParams() {
+    // Retrieve potentially given map and print layout parameter from URL
+    // (e.g. from a bookmarked selection)
+    const searchParams = new URLSearchParams(window.location.search);
 
-let center = [966253.1800856147, 6344703.99262965];
-if (centerArg != null) {
-    const centerCoords = centerArg.split(",");
-    center = [parseFloat(centerCoords[0]), parseFloat(centerCoords[1])];
-}
+    const centerArg = searchParams.get("center");
+    const zoomArg = searchParams.get("zoom");
+    const baselayerArg = searchParams.get("layer");
+    const orientationArg = searchParams.get("orientation");
+    const formatArg = searchParams.get("format");
 
-// type BaselayerType = "OSM" | "ESRI:World_Imagery"
-let activeBaselayer;
-if (baselayerArg != null) {
-    switch (baselayerArg) {
-        case "ESRI:World_Imagery":
-            activeBaselayer = baselayerArg;
-            break;
-        default:
-            activeBaselayer = "OSM";
+    let center = [966253.1800856147, 6344703.99262965];
+    if (centerArg != null) {
+        const centerCoords = centerArg.split(",");
+        if (centerCoords.length === 2) {
+            center = [parseFloat(centerCoords[0]), parseFloat(centerCoords[1])] || center;
+        }
     }
+
+    let zoom = 15;
+    if (zoomArg != null) {
+        zoom = Number(zoomArg) || zoom;
+    }
+
+    // type BaselayerType = "OSM" | "ESRI:World_Imagery"
+    let activeBaselayer = "OSM";
+    if (baselayerArg != null) {
+        switch (baselayerArg) {
+            case "ESRI:World_Imagery":
+                activeBaselayer = baselayerArg;
+                break;
+            default:
+                activeBaselayer = "OSM";
+        }
+    }
+
+    let orientation = ORIENTATION.LANDSCAPE;
+    if (orientationArg != null) {
+        orientation = ORIENTATION[orientationArg.toUpperCase()] || orientation;
+    }
+
+    let format = PAPER_FORMAT.A4;
+    if (formatArg != null) {
+        format = PAPER_FORMAT[formatArg.toUpperCase()] || format;
+        // TODO handle A0
+    }
+    return {
+        center, zoom, activeBaselayer, orientation, format,
+    };
 }
 
-const map = createMap("map", center, 15, activeBaselayer);
+const {
+    center, zoom, activeBaselayer, orientation, format,
+} = getSanitizedUrlSearchParams();
 
-const printLayoutControl = addPrintLayoutControl(map);
+setAllQueryParam({
+    center, zoom, orientation, format,
+});
+
+const map = createMap("map", center, zoom, activeBaselayer);
+const printLayoutControl = addPrintLayoutControl(map, format, orientation);
 const messageController = new MessageController();
 
 bindFormToPrintLayoutControl(printLayoutControl, messageController);
