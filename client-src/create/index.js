3249f7bf54caf0dcf570c34bc6e18cc7e993f242
Matthias Schaub <matthias.schaub@heigit.org>
2025-10-01 13:17:10 +0200

    refactor: move getSanitizedUrlSearchParams to urlHelpers.js

--- client-src/create/index.js
+++ client-src/create/index.js
@@ -4,7 +4,6 @@ import "ol-geocoder/dist/ol-geocoder.css";
 import "./geocoder.css";
 import "./create.css";
 
-import { PAPER_FORMAT, ORIENTATION } from "@giscience/ol-print-layout-control";
 import {
     addGeocoderControl,
     addLayerswitcherControl,
@@ -13,61 +12,8 @@ import {
 } from "./map.js";
 import { bindFormToLayerSwitcherControl, bindFormToPrintLayoutControl } from "./form.js";
 import { MessageController } from "./messageController";
-import { setAllQueryParam } from "../shared";
+import { setAllQueryParam, getSanitizedUrlSearchParams } from "../shared";
 
-function getSanitizedUrlSearchParams() {
-    // Retrieve potentially given map and print layout parameter from URL
-    // (e.g. from a bookmarked selection)
-    //
-    // TODO: Move default values to own module or configuration file
-    //
-    const searchParams = new URLSearchParams(window.location.search);
-
-    const centerArg = searchParams.get("center");
-    const zoomArg = searchParams.get("zoom");
-    const baselayerArg = searchParams.get("layer");
-    const orientationArg = searchParams.get("orientation");
-    const formatArg = searchParams.get("format");
-
-    let center = [966253.1800856147, 6344703.99262965];
-    if (centerArg != null) {
-        const centerCoords = centerArg.split(",");
-        if (centerCoords.length === 2) {
-            center = [parseFloat(centerCoords[0]), parseFloat(centerCoords[1])] || center;
-        }
-    }
-
-    let zoom = 15;
-    if (zoomArg != null) {
-        zoom = Number(zoomArg) || zoom;
-    }
-
-    // type BaselayerType = "OSM" | "ESRI:World_Imagery"
-    let layer = "OSM";
-    if (baselayerArg != null) {
-        switch (baselayerArg) {
-            case "ESRI:World_Imagery":
-                layer = baselayerArg;
-                break;
-            default:
-                layer = "OSM";
-        }
-    }
-
-    let orientation = ORIENTATION.LANDSCAPE;
-    if (orientationArg != null) {
-        orientation = ORIENTATION[orientationArg.toUpperCase()] || orientation;
-    }
-
-    let format = PAPER_FORMAT.A4;
-    if (formatArg != null) {
-        format = PAPER_FORMAT[formatArg.toUpperCase()] || format;
-        // TODO handle A0
-    }
-    return {
-        center, zoom, layer, orientation, format,
-    };
-}
 
 const {
     center, zoom, layer, orientation, format,
