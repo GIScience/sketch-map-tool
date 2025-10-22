06a564c54aedc2bc538e9f28077d9605285e6643
Matthias Schaub <matthias.schaub@heigit.org>
2025-10-22 09:02:41 +0200

    tests: differentiate get attribution tests by availability of api key

--- tests/unit/test_definitions.py
+++ tests/unit/test_definitions.py
@@ -1,7 +1,15 @@
+import os
+
+import pytest
 import pytest_approval
 
 from sketch_map_tool import definitions
 from tests import vcr_app as vcr
+
+
+@pytest.fixture(params=["osm", "oam:59e62beb3d6412ef7220c58e"])
+def layer(request):
+    return request.param
 
 
 def test_get_literatur_references():
@@ -9,6 +17,21 @@ def test_get_literatur_references():
     for r in result:
         assert r.img_src is not None
         assert r.citation is not None
+
+
+@vcr.use_cassette
+def test_get_attribution_esri_api_key_unset(monkeypatch):
+    monkeypatch.setattr("sketch_map_tool.definitions.get_config_value", lambda _: "")
+    pytest_approval.verify(definitions.get_attribution("esri-world-imagery"))
+
+
+@vcr.use_cassette
+@pytest.mark.skipif(
+    os.getenv("CI") == "true",
+    reason="Detected CI environment. ESRI API Key not set in CI.",
+)
+def test_get_attribution_esri_api_key_set():
+    pytest_approval.verify(definitions.get_attribution("esri-world-imagery"))
 
 
 @vcr.use_cassette
