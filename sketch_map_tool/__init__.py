3b4baca3de507203cf979bfa8307ea9329d33034
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-16 07:57:26 +0200

    release 2026-6-16

--- sketch_map_tool/__init__.py
+++ sketch_map_tool/__init__.py
@@ -13,7 +13,7 @@ from sketch_map_tool.config import CONFIG
 from sketch_map_tool.database import client_flask as db_client
 from sketch_map_tool.definitions import LANGUAGES
 
-__version__ = "2026.2.17"
+__version__ = "2026.6.16"
 
 # Setup logging
 LEVEL = getattr(logging, CONFIG.log_level.upper())
